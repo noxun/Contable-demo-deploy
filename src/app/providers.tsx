2baa3130929d4857e89cc133e65902cde8b52580
rats4final <rats4final@gmail.com>
2024-05-06 23:45:26 -0400

    removido codigo duplicado

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -6,17 +6,10 @@ import { ThemeProvider } from "@/components/theme-provider";
 export function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = new QueryClient();
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
       <NextUIProvider>
         <QueryClientProvider client={queryClient}>
           {children}
         </QueryClientProvider>
       </NextUIProvider>
-    </ThemeProvider>
   );
 }
