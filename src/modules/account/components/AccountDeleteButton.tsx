0292a31fd61d773435aa61f302eff26a564f82ba
rats4final <rats4final@gmail.com>
2024-05-07 15:13:40 -0400

    componente para borrar cuentas

---
+++ src/modules/account/components/AccountDeleteButton.tsx
@@ -0,0 +1,77 @@
+"use client"
+import { PropsWithChildren } from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import { toast } from "sonner";
+import { Button } from "@/components/ui/button";
+
+type AccountDeleteButtonProps = {
+  message: string;
+  accountId: number;
+};
+
+export default function AccountDeleteButton({
+  children,
+  message,
+  accountId,
+}: PropsWithChildren & AccountDeleteButtonProps) {
+  const token = localStorage.getItem("token");
+  const queryClient = useQueryClient();
+  const deleteAccountMutation = useMutation({
+    mutationFn: async (id: number) => {
+      const response = await axios.delete(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/Account?accountId=${id}`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      return response.data;
+    },
+    onSuccess: () => {
+      toast("Account deleted succesfully");
+      queryClient.invalidateQueries({ queryKey: ["accounts"] });
+    },
+    onError: (error, variables, context) => {
+      console.log(error, variables, context);
+      toast(error.message);
+    },
+  });
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button>{children}</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Esta absolutamente seguro?</AlertDialogTitle>
+          <AlertDialogDescription>{message}</AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancelar</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={() => {
+              deleteAccountMutation.mutate(accountId);
+            }}
+          >
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
