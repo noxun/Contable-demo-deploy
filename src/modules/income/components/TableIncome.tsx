8fe0b1d39dbef017fe8e647f09b1fe09fdcc285a
rats4final <rats4final@gmail.com>
2024-05-16 16:02:14 -0400

    agregado boton para borrar ingresos

--- src/modules/income/components/TableIncome.tsx
+++ src/modules/income/components/TableIncome.tsx
@@ -26,6 +26,22 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { Button } from "@/components/ui/button";
 import { MoreHorizontal } from "lucide-react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { PropsWithChildren } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "sonner";
+import axios from "axios";
+import Link from "next/link";
 
 const columns: ColumnDef<IIncomeResponse>[] = [
   {
@@ -52,29 +68,82 @@ const columns: ColumnDef<IIncomeResponse>[] = [
     id: "actions",
     enableHiding: false,
     cell: ({ row }) => {
+
       const income = row.original;
       return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" className="h-8 w-8 p-0">
-              <span className="sr-only">Abrir menu</span>
-              <MoreHorizontal className="h-4 w-4" />
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuLabel>Opciones</DropdownMenuLabel>
-            <DropdownMenuItem
-            // onClick={() => navigator.clipboard.writeText(payment.id)}
-            >
-              Editar
-            </DropdownMenuItem>
-            <DropdownMenuItem>Eliminar</DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+        <AlertDialog>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" className="h-8 w-8 p-0">
+                <span className="sr-only">Abrir menu</span>
+                <MoreHorizontal className="h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Opciones</DropdownMenuLabel>
+              <DropdownMenuItem asChild>
+                <Link href={`/dashboard/income/${income.id}/edit`}>Editar</Link>
+              </DropdownMenuItem>
+              <AlertDialogTrigger asChild>
+                <DropdownMenuItem>
+                  <span>Eliminar</span>
+                </DropdownMenuItem>
+              </AlertDialogTrigger>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete the
+                income with id {income.id} and remove the data from our servers.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction asChild><IncomeDeleteButton id={income.id}>Eliminar</IncomeDeleteButton></AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
       );
     },
   },
 ];
+
+function IncomeDeleteButton({children, id}:PropsWithChildren & { id: number }) {
+  const token = localStorage.getItem("token");
+  const queryClient = useQueryClient()
+  const incomeDeleteMutation = useMutation({
+    mutationFn: async (id: number) => {
+      const response = await axios.delete(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/Voucher?id=${id}&type=2`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      return response.data;
+    },
+    onSuccess: () => {
+      toast.success("Ingreso borrado exitosamente")
+      queryClient.invalidateQueries({queryKey:["VoucherIncome"]})
+    },
+    onError: () => {}
+  })
+
+  return (
+    <Button onClick={() => {
+      incomeDeleteMutation.mutate(id);
+    }}>
+      {children}
+    </Button>
+  );
+}
+
+
 
 interface Props {
   data: IIncomeResponse[];
