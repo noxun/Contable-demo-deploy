80ae2d13ed7b710e74baf442f148a4cfb15c4d5a
rats4final <rats4final@gmail.com>
2024-05-17 11:26:56 -0400

    Agregado modal para agregar mas items a un voucher en particular

--- src/modules/income/components/FormEditIncomeItems.tsx
+++ src/modules/income/components/FormEditIncomeItems.tsx
@@ -31,6 +31,27 @@ import { CircularProgress } from "@nextui-org/react";
 import { z } from "zod";
 import { IBank } from "@/modules/banks/interface/banks";
 import { toast } from "sonner";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 
 const accountsSchema = z.object({
   id: z.number(),
@@ -90,56 +111,265 @@ export const FormEditIncomeItems = (props: Props) => {
   const queryClient = useQueryClient();
 
   const deleteIncomeItemMutation = useMutation({
-    mutationFn: async (incomeItemId:number) => {
-      const response = await axios.delete(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/Voucher/Item?id=${incomeItemId}&type=2`, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      })
+    mutationFn: async (incomeItemId: number) => {
+      const response = await axios.delete(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/Voucher/Item?id=${incomeItemId}&type=2`,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
       return response;
     },
     onSuccess: () => {
-      queryClient.invalidateQueries({queryKey: ["VoucherIncome"]})
-      queryClient.invalidateQueries({queryKey: ["VoucherIncome", voucherId.toString()]})
+      queryClient.invalidateQueries({ queryKey: ["VoucherIncome"] });
+      queryClient.invalidateQueries({
+        queryKey: ["VoucherIncome", voucherId.toString()],
+      });
       toast.success("Elemento eliminado correctamente");
     },
     onError: (error) => {
-      console.log(error)
-      toast.error("Error al editar el elemento")
-    }
-  })
+      console.log(error);
+      toast.error("Error al editar el elemento");
+    },
+  });
 
-  function removeIncomeItem(incomeItemId: number) {
+  const editIncomeItemMutation = useMutation({
+    mutationFn: async (incomeItem: IIncomeItem) => {
+      const response = await axios.put(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/Voucher/Item?type=2`,
+        incomeItem,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      return response.data;
+    },
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["VoucherIncome"] });
+      queryClient.invalidateQueries({
+        queryKey: ["VoucherIncome", voucherId.toString()],
+      });
+      toast.success("Elemento editado correctamente");
+    },
+    onError: (error) => {
+      console.log(error);
+      toast.error("Error al editar el elemento");
+    },
+  });
+
+  function removeIncomeItem(index: number, incomeItemId: number) {
+    const updateIncomeItems = incomeItems.filter((_, i) => i != index);
+    setIncomeItems([...updateIncomeItems]);
     deleteIncomeItemMutation.mutate(incomeItemId);
   }
 
   console.log(incomeItems);
 
+  const addIncomeItemSchema = z.object({
+    debitBs: z.coerce.number(),
+    debitSus: z.number().optional().default(0),
+    assetBs: z.coerce.number(),
+    assetSus: z.number().optional().default(0),
+    gloss: z.string(),
+    accountId: z.number(),
+    voucherId: z.number(),
+  });
+
+  type NewIncomeItem = z.infer<typeof addIncomeItemSchema>;
+
+  const addIncomeItemForm = useForm<NewIncomeItem>({
+    resolver: zodResolver(addIncomeItemSchema),
+    defaultValues: {
+      voucherId: voucherId,
+    },
+  });
+
+  const addIncomeItemMutation = useMutation({
+    mutationFn: async (data: NewIncomeItem) => {
+      const response = await axios.post(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/Voucher/Item?type=2&voucherId=${data.voucherId}`,
+        [data],
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      return response;
+    },
+    onSuccess: () => {
+      queryClient.resetQueries({queryKey:["VoucherIncome", voucherId.toString()]})
+      queryClient.invalidateQueries({queryKey:["VoucherIncome"]});
+      toast.success("item creado correctamente");
+    },
+    onError: (error) => {
+      console.log(error);
+      toast.error("Hubo un error al crear el item");
+    },
+  });
+
+  function onSubmit(values: NewIncomeItem) {
+    console.log(values);
+    addIncomeItemMutation.mutate(values);
+    //setIncomeItems((previousItems) => [...previousItems, values]);
+  }
+
   return (
     <div>
       <div className="flex justify-between items-center mb-2">
         <h2 className="text-lg font-medium">Asiento contable</h2>
-        <Button
-          type="button"
-          onClick={() =>
-            setIncomeItems((a) => [
-              ...a,
-              {
-                debitBs: "",
-                debitSus: "",
-                assetBs: "",
-                assetSus: "",
-                gloss: "",
-                accountId: "",
-                voucherId: "",
-              },
-            ])
-          }
-        >
-          <span className="mr-2">Adicionar Item</span>
-          <Plus size={18} />
-        </Button>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button>
+              <span className="mr-2">Adicionar Item</span>
+              <Plus size={18} />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[600px]">
+            <DialogHeader>
+              <DialogTitle>Adicionar item</DialogTitle>
+              <DialogDescription>
+                Agrega un nuevo item al ingreso actual
+              </DialogDescription>
+            </DialogHeader>
+            <div>
+              <Form {...addIncomeItemForm}>
+                <form
+                  onSubmit={addIncomeItemForm.handleSubmit(onSubmit)}
+                  className="flex"
+                >
+                  <FormField
+                    control={addIncomeItemForm.control}
+                    name="accountId"
+                    render={({ field }) => (
+                      <FormItem className="flex flex-col">
+                        <FormLabel>Account</FormLabel>
+                        <Popover>
+                          <PopoverTrigger asChild>
+                            <FormControl>
+                              <Button
+                                variant="outline"
+                                role="combobox"
+                                className={cn(
+                                  "w-[200px] justify-between",
+                                  !field.value && "text-muted-foreground"
+                                )}
+                              >
+                                {field.value
+                                  ? data.data.find(
+                                      (account) => account.id === field.value
+                                    )?.description
+                                  : "Select Account"}
+                                <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                              </Button>
+                            </FormControl>
+                          </PopoverTrigger>
+                          <PopoverContent className="w-[200px] p-0">
+                            <Command>
+                              <CommandInput placeholder="Search language..." />
+                              <CommandEmpty>No language found.</CommandEmpty>
+                              <CommandGroup>
+                                {data.data.map((account) => (
+                                  <CommandItem
+                                    value={account.id.toString()}
+                                    key={account.id}
+                                    onSelect={() => {
+                                      addIncomeItemForm.setValue(
+                                        "accountId",
+                                        account.id
+                                      );
+                                    }}
+                                  >
+                                    <Check
+                                      className={cn(
+                                        "mr-2 h-4 w-4",
+                                        account.id === field.value
+                                          ? "opacity-100"
+                                          : "opacity-0"
+                                      )}
+                                    />
+                                    {account.description}
+                                  </CommandItem>
+                                ))}
+                              </CommandGroup>
+                            </Command>
+                          </PopoverContent>
+                        </Popover>
+                        <FormDescription>
+                          This is the language that will be used in the
+                          dashboard.
+                        </FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={addIncomeItemForm.control}
+                    name="debitBs"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Debe Bs</FormLabel>
+                        <FormControl>
+                          <Input placeholder="debe" {...field} />
+                        </FormControl>
+                        <FormDescription>
+                          This is your public display name.
+                        </FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={addIncomeItemForm.control}
+                    name="assetBs"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Haber Bs:</FormLabel>
+                        <FormControl>
+                          <Input placeholder="shadcn" {...field} />
+                        </FormControl>
+                        <FormDescription>
+                          This is your public display name.
+                        </FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={addIncomeItemForm.control}
+                    name="gloss"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Glosa</FormLabel>
+                        <FormControl>
+                          <Input placeholder="shadcn" {...field} />
+                        </FormControl>
+                        <FormDescription>
+                          This is your public display name.
+                        </FormDescription>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <Button type="submit">Submit</Button>
+                </form>
+              </Form>
+            </div>
+            {/* <DialogFooter>
+              <DialogClose asChild>
+                <Button>Cerrar</Button>
+              </DialogClose>
+            </DialogFooter> */}
+          </DialogContent>
+        </Dialog>
       </div>
       <Table>
         <TableHeader>
@@ -208,7 +438,9 @@ export const FormEditIncomeItems = (props: Props) => {
                   variant="secondary"
                   size="icon"
                   type="button"
-                  onClick={() => { }}
+                  onClick={() => {
+                    editIncomeItemMutation.mutate(item);
+                  }}
                 >
                   <Pencil size={16} />
                 </Button>
@@ -217,7 +449,7 @@ export const FormEditIncomeItems = (props: Props) => {
                   size="icon"
                   type="button"
                   onClick={() => {
-                    removeIncomeItem(item.id)
+                    removeIncomeItem(index, item?.id);
                   }}
                 >
                   <Trash2 size={16} />
