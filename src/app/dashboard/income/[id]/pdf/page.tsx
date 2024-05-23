c97eca66ce73345b02091f7cb20608b7c13f2cc2
Jose Mendoza <joseemendozaan1@gmail.com>
2024-05-23 09:30:02 -0400

    pdf mejorado

--- src/app/dashboard/income/[id]/pdf/page.tsx
+++ src/app/dashboard/income/[id]/pdf/page.tsx
@@ -1,27 +1,34 @@
-"use client"
+"use client";
 import {
   Page,
   Text,
   View,
   Document,
   StyleSheet,
+  Image,
   PDFViewer,
+  Font
 } from "@react-pdf/renderer";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { createTw } from "react-pdf-tailwind";
 
+import myImage from '@/app/dashboard/income/[id]/pdf/noxun.jpg'; 
 const tw = createTw({
   theme: {
-    extend: {
-    },
+    extend: {},
   },
 });
-
-export default function PdfPage({ params }: { params: { id: string }}) {
-
+Font.register({
+  family: 'Open Sans',
+  fonts: [
+    { src: 'https://cdn.jsdelivr.net/npm/open-sans-all@0.1.3/fonts/open-sans-regular.ttf' },
+    { src: 'https://cdn.jsdelivr.net/npm/open-sans-all@0.1.3/fonts/open-sans-600.ttf', fontWeight: 600 }
+  ]
+})
+export default function PdfPage({ params }: { params: { id: string } }) {
   const token = localStorage.getItem("token");
-  const {id} = params;
+  const { id } = params;
 
   const getSingleIncomeQuery = useQuery({
     queryKey: ["VoucherIncome", id],
@@ -35,14 +42,13 @@ export default function PdfPage({ params }: { params: { id: string }}) {
           },
         }
       );
+      console.log(response.data);
       return response.data;
     },
-  })
+  });
 
   if (getSingleIncomeQuery.isLoading) {
-    return (
-      <div>Loading...</div>
-    )
+    return <div>Loading...</div>;
   }
 
   const { items } = getSingleIncomeQuery.data;
@@ -55,55 +61,76 @@ export default function PdfPage({ params }: { params: { id: string }}) {
   return (
     <PDFViewer showToolbar={true} className="h-full">
       <Document>
-        <Page wrap={true} size="A4" style={tw("p-4 flex gap-8")}>
-          <View style={tw("flex justify-center items-start")}>
-            <View style={tw("flex flex-col items-center justify-center")}>
-              <Text>NOXUN</Text>
-              <Text>Gestion {new Date().getFullYear()}</Text>
-            </View>
-          </View>
-          <View style={tw("flex flex-col items-center justify-center")}>
-              <Text>COMPROBANTE DE EGRESO N (Numero Aca)</Text>
-              <Text>(Expresado en Bs)</Text>
-          </View>
-          <View>
-            <Text>Fecha: {getSingleIncomeQuery?.data?.canceledTo}</Text>
-            <Text>Section #1 {getSingleIncomeQuery?.data?.gloss}</Text>
-            <Text>Section #2</Text>
-          </View>
-          <View style={tw("mt-4")}>
-            <Text style={tw("font-bold mb-2")}>Items contables:</Text>
-            <View style={tw("flex flex-col border border-gray-500")}>
-              <View style={tw("flex flex-row border-b border-gray-500 bg-gray-200")}>
-                <Text style={tw("flex-1 p-2 border-r border-gray-500")}>ID</Text>
-                <Text style={tw("flex-1 p-2 border-r border-gray-500")}>Debit Bs</Text>
-                <Text style={tw("flex-1 p-2 border-r border-gray-500")}>Debit Sus</Text>
-                <Text style={tw("flex-1 p-2 border-r border-gray-500")}>Asset Bs</Text>
-                <Text style={tw("flex-1 p-2 border-r border-gray-500")}>Asset Sus</Text>
-                <Text style={tw("flex-1 p-2 border-r border-gray-500")}>Gloss</Text>
-                <Text style={tw("flex-1 p-2")}>Account ID</Text>
-              </View>
-              {items.map((item) => (
-                <View key={item.id} style={tw("flex flex-row border-b border-gray-500")}>
-                  <Text style={tw("flex-1 p-2 border-r border-gray-500")}>{item.id}</Text>
-                  <Text style={tw("flex-1 p-2 border-r border-gray-500")}>{item.debitBs}</Text>
-                  <Text style={tw("flex-1 p-2 border-r border-gray-500")}>{item.debitSus.toFixed(2)}</Text>
-                  <Text style={tw("flex-1 p-2 border-r border-gray-500")}>{item.assetBs}</Text>
-                  <Text style={tw("flex-1 p-2 border-r border-gray-500")}>{item.assetSus.toFixed(2)}</Text>
-                  <Text style={tw("flex-1 p-2 border-r border-gray-500")}>{item.gloss}</Text>
-                  <Text style={tw("flex-1 p-2")}>{item.accountId}</Text>
-                </View>
-              ))}
-              <View style={tw("flex flex-row bg-gray-200")}>
-                <Text style={tw("flex-1 p-2 border-r border-gray-500")}>Total</Text>
-                <Text style={tw("flex-1 p-2 border-r border-gray-500")}>{totalDebitBs}</Text>
-                <Text style={tw("flex-1 p-2 border-r border-gray-500")}>{totalDebitSus.toFixed(2)}</Text>
-                <Text style={tw("flex-1 p-2 border-r border-gray-500")}>{totalAssetBs}</Text>
-                <Text style={tw("flex-1 p-2 border-r border-gray-500")}>{totalAssetSus.toFixed(2)}</Text>
-                <Text style={tw("flex-1 p-2 border-r border-gray-500")}></Text>
-                <Text style={tw("flex-1 p-2")}></Text>
+        <Page size="A4" style={tw("p-4 flex flex-col h-full text-base")}>
+          {/* <Page wrap={true} size="A4" style={tw("p-4 flex gap-8")}> */}
+          <View style={tw("border-4 border-black m-0 p-4 grid grid-rows-[auto_auto_auto_1fr_auto] h-full")}>
+            <View style={tw("flex justify-center items-start")}>
+              <View style={tw("flex flex-col items-center justify-center")}>
+                <Image src={myImage.src} style={tw("p-1 w-15 h-20")} />
+                <Text >NOXUN</Text>
+                <Text>Gestion {new Date().getFullYear()}</Text>
               </View>
             </View>
+            <View style={tw("flex flex-col items-center justify-center mt-8")}>
+              <Text style={tw("text-2xl mt-0 ")}>COMPROBANTE DE EGRESO N (Número Acá)</Text>
+              <Text style={tw("text-xl")}>(Expresado en Bs)</Text>
+            </View>
+            <View>
+              <Text>Fecha: {getSingleIncomeQuery?.data?.canceledTo}</Text>
+              <Text>Pagado a: {getSingleIncomeQuery?.data?.gloss}</Text>
+              {/* <Text>Section #2</Text> */}
+            </View>
+            <View style={tw("mt-4 flex-1")}>
+      <Text style={tw("font-bold mb-2")}>Items contables:</Text>
+      <View style={tw("flex flex-col h-full border border-gray-500")}>
+      <View style={tw("flex flex-row border-b border-gray-500 bg-gray-200")}>
+        <Text style={tw("w-1/12 p-2 border-r border-gray-500 text-center")}>ID</Text>
+        <Text style={tw("w-3/12 p-2 border-r border-gray-500 text-center")}>Glosa</Text>
+        <Text style={tw("w-2/12 p-2 border-r border-gray-500 text-center")}>Debe Bs</Text>
+        <Text style={tw("w-2/12 p-2 border-r border-gray-500 text-center")}>Haber Bs</Text>
+        <Text style={tw("w-2/12 p-2 border-r border-gray-500 text-center")}>Debe Sus</Text>
+        <Text style={tw("w-2/12 p-2 border-r border-gray-500 text-center")}>Haber Sus</Text>
+        <Text style={tw("w-2/12 p-2 text-center")}>ID CUENTA</Text>
+      </View>
+      <View style={tw("flex flex-col flex-1")}>
+        {items.map((item) => (
+          <View key={item.id} style={tw("flex flex-row  border-gray-500")}>
+            <Text style={tw("w-1/12 p-2 border-r border-gray-500 text-center")}>{item.id}</Text>
+            <Text style={tw("w-3/12 p-2 border-r border-gray-500 text-center")}>{item.gloss}</Text>
+            <Text style={tw("w-2/12 p-2 border-r border-gray-500 text-center")}>{item.debitBs}</Text>
+            <Text style={tw("w-2/12 p-2 border-r border-gray-500 text-center")}>{item.assetBs}</Text>
+            <Text style={tw("w-2/12 p-2 border-r border-gray-500 text-center")}>{item.debitSus.toFixed(2)}</Text>
+            <Text style={tw("w-2/12 p-2 border-r border-gray-500 text-center")}>{item.assetSus.toFixed(2)}</Text>
+            <Text style={tw("w-2/12 p-2 text-center")}>{item.accountId}</Text>
+          </View>
+        ))}
+      </View>
+      <View style={tw("flex flex-row border-t border-gray-500 bg-gray-200")}>
+        <Text style={tw("w-4/12 p-2 border-r border-gray-500 text-center")}>Total</Text>
+        {/* <Text style={tw("w-1/12 p-2 border-r border-gray-500")}></Text> */}
+        <Text style={tw("w-2/12 p-2 border-r border-gray-500 text-center")}>{totalDebitBs}</Text>
+        <Text style={tw("w-2/12 p-2 border-r border-gray-500 text-center")}>{totalAssetBs}</Text>
+        <Text style={tw("w-2/12 p-2 border-r border-gray-500 text-center")}>{totalDebitSus.toFixed(2)}</Text>
+        <Text style={tw("w-2/12 p-2 border-r border-gray-500 text-center")}>{totalAssetSus.toFixed(2)}</Text>
+        <Text style={tw("w-2/12 p-2")}></Text>
+      </View>
+    </View>
+    </View>
+            <View style={tw("flex flex-row justify-between mt-4")}>
+              <View style={tw("text-center border border-gray-500 rounded-md h-32 w-60 flex flex-col justify-end px-4 pb-2 mr-2")}>
+                <View style={tw("border-b border-gray-500 mb-2 w-full")}></View>
+                <Text>ELABORADO POR</Text>
+              </View>
+              <View style={tw("text-center border border-gray-500 rounded-md h-32 w-60 flex flex-col justify-end px-4 pb-2 mr-2")}>
+                <View style={tw("border-b border-gray-500 mb-2 w-full")}></View>
+                <Text>REVISADO POR</Text>
+              </View>
+              <View style={tw("text-center border border-gray-500 rounded-md h-32 w-60 flex flex-col justify-end px-4 pb-2 ")}>
+                <View style={tw("border-b border-gray-500 mb-2 w-full")}></View>
+                <Text>APROBADO POR</Text>
+              </View>
+            </View>
+
           </View>
         </Page>
       </Document>
