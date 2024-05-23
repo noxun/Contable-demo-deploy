f4533ef0fe2a493431f79238081e810d3eba0f3d
David Antezana, rats4final <rats4final@gmail.com>
2024-05-23 00:36:35 -0400

    David: agregado el nuevo componente reusable a la pagina creacion de egresos

--- src/app/dashboard/expenses/new/page.tsx
+++ src/app/dashboard/expenses/new/page.tsx
@@ -1,10 +1,11 @@
-import FormNewIncome from "@/modules/income/components/FormNewIncome";
+import FormNewVoucher from "@/modules/shared/components/FormNewVoucher";
+import { VoucherType, VoucherTypeRoute } from "@/modules/shared/types/sharedTypes";
 
 export default function NewExpensePage() {
   return (
     <div className="px-6">
       <h2 className="text-lg font-bold">Formulario para crear egreso</h2>
-      <FormNewIncome type="1" />
+      <FormNewVoucher routeType={VoucherTypeRoute.EXPENSE} type={VoucherType.EXPENSE}/>
     </div>
   );
 };
