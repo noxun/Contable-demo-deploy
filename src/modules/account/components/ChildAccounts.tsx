4ebcb3d1022d657dfcb1acffab14e7616113a1fe
David Antezana, rats4final <rats4final@gmail.com>
2024-06-03 05:22:16 -0400

    modificado listado de cuentas para que use un dropdown

--- src/modules/account/components/ChildAccounts.tsx
+++ src/modules/account/components/ChildAccounts.tsx
@@ -1,50 +1,92 @@
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 import AccountCreateButton from "./AccountCreateButton";
 import AccountDeleteButton from "./AccountDeleteButton";
 import AccountEditButton from "./AccountEditButton";
 import { Account } from "../types/account";
-import { useState } from "react";
+import { MoreHorizontal } from "lucide-react";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
 
 type ChildAccountsProps = {
-  accounts: Account[] | undefined
-}
+  accounts: Account[] | undefined;
+};
 
 export default function ChildAccounts({ accounts }: ChildAccountsProps) {
-
   const getChildCodes = (account: Account): string[] => {
     const childCodes = account.accountChild.map((child) => child.code);
-    const grandChildCodes = account.accountChild.flatMap((child) => getChildCodes(child));
+    const grandChildCodes = account.accountChild.flatMap((child) =>
+      getChildCodes(child)
+    );
     return [...childCodes, ...grandChildCodes];
   };
 
   return (
-    <Accordion type="multiple" className="w-full" defaultValue={accounts?.flatMap((account) => [account.code, ...getChildCodes(account)]) ?? []}>
+    <Accordion
+      type="multiple"
+      className="w-full"
+      defaultValue={
+        accounts?.flatMap((account) => [
+          account.code,
+          ...getChildCodes(account),
+        ]) ?? []
+      }
+    >
       {accounts?.map((account, index) => (
         <AccordionItem key={index} value={account.code} className="px-2 border">
-          <AccordionTrigger>
-            {account.code}:{account.description}
-          </AccordionTrigger>
-          <AccordionContent>
-            <div>
-              {account.accountChild.length > 0 && (
-                <ChildAccounts accounts={account.accountChild} />
-              )}
-              <div className="flex items-center justify-end gap-4 pt-4">
-                <AccountCreateButton fatherId={account.id}>
-                  Nuevo Registro
-                </AccountCreateButton>
-                <AccountEditButton account={account}>
-                  Editar Registro
-                </AccountEditButton>
-                <AccountDeleteButton
-                  accountId={account.id}
-                  message="This action cannot be undone. This will
+          <div className="flex items-center gap-2">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="h-8 w-8 p-0">
+                  <span className="sr-only">Abrir Menu de Opciones</span>
+                  <MoreHorizontal className="h-4 w-4" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="flex flex-col gap-1">
+                <DropdownMenuLabel>Acciones</DropdownMenuLabel>
+                <DropdownMenuItem asChild>
+                  <AccountCreateButton fatherId={account.id}>
+                    Nuevo Registro
+                  </AccountCreateButton>
+                </DropdownMenuItem>
+                <DropdownMenuItem asChild>
+                  <AccountEditButton account={account}>
+                    Editar Registro
+                  </AccountEditButton>
+                </DropdownMenuItem>
+                <DropdownMenuItem asChild>
+                  <AccountDeleteButton
+                    accountId={account.id}
+                    message="This action cannot be undone. This will
                               permanently delete the account and remove the
                               data from our servers."
-                >
-                  Borrar Registro
-                </AccountDeleteButton>
-              </div>
+                  >
+                    Borrar Registro
+                  </AccountDeleteButton>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+            <AccordionTrigger>
+              {account.code}:{account.description}
+            </AccordionTrigger>
+          </div>
+          <AccordionContent>
+            <div>
+              {account.accountChild!.length > 0 && (
+                <ChildAccounts accounts={account.accountChild} />
+              )}
             </div>
           </AccordionContent>
         </AccordionItem>
