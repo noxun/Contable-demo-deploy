5dfa25038b4c9b507df8a9deaaf5964295d05327
David Antezana, rats4final <rats4final@gmail.com>
2024-06-03 22:45:54 -0400

    David: otros ajustes menores

--- src/app/dashboard/results/accounting-excel.tsx
+++ src/app/dashboard/results/accounting-excel.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Select from "react-select";
 import { addDays, format } from "date-fns";
-import { Calendar as CalendarIcon } from "lucide-react";
+import { Calendar as CalendarIcon, FileText, Sheet } from "lucide-react";
 import { DateRange } from "react-day-picker";
 
 import { cn } from "@/lib/utils";
@@ -14,12 +14,16 @@ import {
 } from "@/components/ui/popover";
 import { useState } from "react";
 import axios from "axios";
+import { es } from "date-fns/locale";
+import { DataTable } from "@/components/ui/data-table";
 
 const options = [
   { value: "balanceDeSumas", label: "Balance De Sumas" },
   { value: "estadoDeResultado", label: "Estado De Resultado" },
   { value: "balanceGeneral", label: "Balance General" },
 ];
+
+//biggerBook
 
 export default function AccountingExcelPage() {
   const [selectType, setSelectType] = useState<string>("");
@@ -55,66 +59,75 @@ export default function AccountingExcelPage() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen p-4 space-y-4 bg-gray-100">
-      <div className="w-72">
-        <Select 
-          options={options} 
-          onChange={(value) => setSelectType(value!.value)}
-          styles={{ container: (provided) => ({ ...provided, width: '100%' }) }}
-        />
-      </div>
-      <div className="w-72">
-        <Popover>
-          <PopoverTrigger asChild>
-            <Button
-              id="date"
-              variant={"outline"}
-              className={cn(
-                "w-full justify-start text-left font-normal",
-                !date && "text-muted-foreground"
-              )}
-            >
-              <CalendarIcon className="mr-2 h-4 w-4" />
-              {date?.from ? (
-                date.to ? (
-                  <>
-                    {format(date.from, "LLL dd, y")} -{" "}
-                    {format(date.to, "LLL dd, y")}
-                  </>
+    <div className="flex flex-col gap-6">
+      <div className="flex items-center justify-evenly">
+        <div className="w-72">
+          <Select
+            options={options}
+            onChange={(value) => setSelectType(value!.value)}
+            styles={{ container: (provided) => ({ ...provided, width: '100%' }) }}
+          />
+        </div>
+        <div className="w-72">
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                id="date"
+                variant={"outline"}
+                className={cn(
+                  "w-full justify-start text-left font-normal",
+                  !date && "text-muted-foreground"
+                )}
+              >
+                <CalendarIcon className="mr-2 h-4 w-4" />
+                {date?.from ? (
+                  date.to ? (
+                    <>
+                      {format(date.from, "LLL dd, y")} -{" "}
+                      {format(date.to, "LLL dd, y")}
+                    </>
+                  ) : (
+                    format(date.from, "LLL dd, y")
+                  )
                 ) : (
-                  format(date.from, "LLL dd, y")
-                )
-              ) : (
-                <span>Pick a date</span>
-              )}
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-auto p-0" align="start">
-            <Calendar
-              initialFocus
-              mode="range"
-              defaultMonth={date?.from}
-              selected={date}
-              onSelect={setDate}
-              numberOfMonths={2}
-            />
-          </PopoverContent>
-        </Popover>
+                  <span>Pick a date</span>
+                )}
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0" align="start">
+              <Calendar
+                initialFocus
+                mode="range"
+                locale={es  }
+                defaultMonth={date?.from}
+                selected={date}
+                onSelect={setDate}
+                numberOfMonths={2}
+              />
+            </PopoverContent>
+          </Popover>
+        </div>
+        {/* <Button onClick={handleClick} disabled={isLoading}>
+          {isLoading ? 'Generando Reporte...' : 'Generar Reporte'}
+        </Button> */}
+        <Button variant="outline" size="icon">
+            <Sheet className="h-4 w-4"/>
+        </Button>
+        <Button variant="outline" size="icon">
+          <FileText className="h-4 w-4"/>
+        </Button>
+        {fileLink && (
+          <a
+            className="mt-4 inline-block px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
+            href={fileLink}
+            rel="noopener noreferrer"
+            download
+          >
+            Descargar Reporte
+          </a>
+        )}
       </div>
-      <Button onClick={handleClick} disabled={isLoading}>
-        {isLoading ? 'Generando Reporte...' : 'Generar Reporte'}
-      </Button>
-      {fileLink && (
-        <a
-          className="mt-4 inline-block px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition"
-          href={fileLink} 
-          target="_blank" 
-          rel="noopener noreferrer" 
-          download
-        >
-          Descargar Reporte
-        </a>
-      )}
+      <DataTable columns={[]} data={[]}/>
     </div>
   );
 }
--- src/app/dashboard/results/page.tsx
+++ src/app/dashboard/results/page.tsx
@@ -3,9 +3,8 @@ import BalanceAmountsPage from "./balance-amounts";
 
 export default function ResultsPage() {
   return (
-    <div className="min-h-screen bg-gray-100 flex items-end justify-center">
+    <div className='w-full'>
       <AccountingExcelPage/>
-      <BalanceAmountsPage/>
     </div>
   );
 }
