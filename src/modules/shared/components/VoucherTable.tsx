41652c6f4ad92ac6ff1eb950a41d39ba4ba6d99c
David Antezana, rats4final <rats4final@gmail.com>
2024-08-26 06:20:39 -0400

    chore: formateado voucher table

--- src/modules/shared/components/VoucherTable.tsx
+++ src/modules/shared/components/VoucherTable.tsx
@@ -1,18 +1,21 @@
-"use client"
+"use client";
 
-import { DataTable } from "@/components/ui/data-table"
-import { Voucher, VoucherType, VoucherTypeRoute } from "../types/sharedTypes"
-import { columns } from "./voucher-columns"
-
+import { DataTable } from "@/components/ui/data-table";
+import { Voucher, VoucherType, VoucherTypeRoute } from "../types/sharedTypes";
+import { columns } from "./voucher-columns";
 
 type VoucherTableProps = {
   data: Voucher[];
-  voucherType: VoucherType
-  voucherTypeRoute: VoucherTypeRoute
-}
+  voucherType: VoucherType;
+  voucherTypeRoute: VoucherTypeRoute;
+};
 
-export default function VoucherTable({data, voucherType, voucherTypeRoute}: VoucherTableProps) {
+export default function VoucherTable({
+  data,
+  voucherType,
+  voucherTypeRoute,
+}: VoucherTableProps) {
   return (
-    <DataTable columns={columns(voucherType, voucherTypeRoute)} data={data}/>
-  )
+    <DataTable columns={columns(voucherType, voucherTypeRoute)} data={data} />
+  );
 }
