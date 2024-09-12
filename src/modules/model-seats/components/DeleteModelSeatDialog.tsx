734ffb232a6e6682ec9946a95e5a3dabceefc40c
David Antezana, rats4final <rats4final@gmail.com>
2024-09-12 09:10:28 -0400

    wip: edicion y eliminacion de asientos modelo

---
+++ src/modules/model-seats/components/DeleteModelSeatDialog.tsx
@@ -0,0 +1,49 @@
+"use client";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { useMutation } from "@tanstack/react-query";
+import { Trash } from "lucide-react";
+
+export default function DeleteModelSeatDialog() {
+
+  function handleClick() {
+    
+  }
+
+  // WIP 
+  // const deleteModelSeatMutation = useMutation({
+  //   mutationFn: async () => {}
+  // })
+
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="outline" size="icon"> 
+          <Trash className="size-4"/>
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Esta Seguro?</AlertDialogTitle>
+          <AlertDialogDescription>
+            Esta Accion borrara el modelo seleccionado y sus elementos
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancelar</AlertDialogCancel>
+          <AlertDialogAction onClick={handleClick}>Continuar</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
---
+++ src/modules/model-seats/components/EditModelSeatButton.tsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { ModelSeat } from "@/lib/types";
+import { Pencil } from "lucide-react";
+import Link from "next/link";
+
+type EditModelSeatButtonProps = {
+  modelSeat: ModelSeat;
+};
+
+export default function EditModelSeatButton({
+  modelSeat,
+}: EditModelSeatButtonProps) {
+  return (
+    <Button asChild size="icon" variant="outline">
+      <Link href={`/dashboard/model-seats/${modelSeat.id}/edit`}>
+        <Pencil className="size-4" />
+      </Link>
+    </Button>
+  );
+}
