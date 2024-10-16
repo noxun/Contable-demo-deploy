f2d813b0f3ae61a20d15204b7cef199332ae1a8b
David Antezana, rats4final <rats4final@gmail.com>
2024-10-16 00:55:05 -0400

    wip(extracts): Agregadas mas columnas y botones a la tabla

--- src/modules/banks/components/bank-excerpt-data-columns.tsx
+++ src/modules/banks/components/bank-excerpt-data-columns.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { BankExcerptData } from "@/lib/types";
 import { ColumnDef } from "@tanstack/react-table";
+import { PlaneIcon, Save, SaveAll } from "lucide-react";
 import Select from 'react-select'
 
 
@@ -9,6 +11,10 @@ export const columns: ColumnDef<BankExcerptData>[] = [
   {
     accessorKey: "date",
     header: "Fecha",
+  },
+  {
+    accessorKey: "description",
+    header: "Descripcion",
   },
   {
     accessorKey: "agency",
@@ -29,5 +35,22 @@ export const columns: ColumnDef<BankExcerptData>[] = [
       const bankExcerptData = row.original
       return <Select/>
     } 
+  },
+  {
+    header: "Asiento Modelo",
+    cell: ({row}) => {
+      const bankExcerptData = row.original
+      return <Select/>
+    } 
+  },
+  {
+    header: "Acciones",
+    cell: ({row}) => {
+      return <div className="flex items-center gap-2">
+        <Button variant="outline" size="icon" title="Guardar como Asiento"><Save/></Button>
+        <Button variant="outline" size="icon" title="Registrar en el trazo"><PlaneIcon/></Button>
+        <Button variant="outline" size="icon" title="Completar Registro"><SaveAll/></Button>
+      </div>
+    }
   }
 ];
