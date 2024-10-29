aadb64b73489fb8bb325dc191632962c24e0f367
David Antezana, rats4final <rats4final@gmail.com>
2024-10-29 18:48:05 -0400

    feat(api): Agregado interceptor para redireccionar al usuario en caso de obtener un 401

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,22 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
   headers: {
     "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    if (error.response && error.response.status === 401) {
+      if (typeof window !== "undefined") {
+        window.location.replace("/auth/login");
+      }
+    }
+    return Promise.reject(error);
   }
-})
+);
