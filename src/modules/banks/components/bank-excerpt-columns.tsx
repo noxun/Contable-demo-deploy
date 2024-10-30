04f42893248c273a23b8588a3e8fdda9b55b9c2a
David Antezana, rats4final <rats4final@gmail.com>
2024-10-30 13:19:27 -0400

    feat(bank-extract-columns): Cambiada la columna a una funcion que acepta el bankId para invalidar la query de registerSeat, y agregada logica para deshabilitar el boton de trazo

--- src/modules/banks/components/bank-excerpt-columns.tsx
+++ src/modules/banks/components/bank-excerpt-columns.tsx
@@ -4,44 +4,45 @@ import { Button } from "@/components/ui/button";
 import { BankExcerpt } from "@/lib/types";
 import { ColumnDef } from "@tanstack/react-table";
 import { Save, PlaneIcon } from "lucide-react";
-import AccountsSelect from "./AccountsSelect";
+import RegisterSeat from "./RegisterSeat";
 
-export const columns: ColumnDef<BankExcerpt>[] = [
-  {
-    accessorKey: "date",
-    header: "Fecha",
-  },
-  {
-    accessorKey: "nroDocument",
-    header: "Numero de Doc.",
-  },
-  {
-    accessorKey: "amount",
-    header: "Monto",
-  },
-  {
-    header: "Cuenta de banco",
-    cell: ({ row }) => {
-      return <AccountsSelect />;
+export function columns(bankId: string | number): ColumnDef<BankExcerpt>[] {
+  return [
+    {
+      accessorKey: "date",
+      header: "Fecha",
     },
-  },
-  {
-    accessorKey: "glossInExtract",
-    header: "Glosa",
-  },
-  {
-    header: "Acciones",
-    cell: ({ row }) => {
-      return (
-        <div className="flex items-center gap-2">
-          <Button variant="outline" size="icon" title="Guardar como Asiento">
-            <Save />
-          </Button>
-          <Button variant="outline" size="icon" title="Registrar en el trazo">
-            <PlaneIcon />
-          </Button>
-        </div>
-      );
+    {
+      accessorKey: "nroDocument",
+      header: "Numero de Doc.",
     },
-  },
-];
+    {
+      accessorKey: "amount",
+      header: "Monto",
+    },
+    {
+      accessorKey: "glossInExtract",
+      header: "Glosa",
+    },
+    {
+      header: "Cuenta de banco",
+      cell: ({ row }) => {
+        const bankExtract = row.original
+        return <RegisterSeat bankExtractId={bankExtract.id} bankId={bankId} hasBeenRegisteredToAccount={bankExtract.accountId !== 0} extractAccountId={bankExtract.accountId}/>;
+      },
+    },
+    {
+      header: "Acciones",
+      cell: ({ row }) => {
+        const bankExtract = row.original
+        return (
+          <div className="flex items-center gap-2">
+            <Button disabled={!bankExtract.accountingEntry && !bankExtract.trazoRegister} variant="outline" size="icon" title="Registrar en el trazo">
+              <PlaneIcon />
+            </Button>
+          </div>
+        );
+      },
+    },
+  ];
+}
