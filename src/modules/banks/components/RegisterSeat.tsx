237aef2e95cab597aca07029ddab129ae0333deb
David Antezana, rats4final <rats4final@gmail.com>
2024-10-30 13:20:18 -0400

    feat(banks): Agregado componente para registrar una contracuenta

---
+++ src/modules/banks/components/RegisterSeat.tsx
@@ -0,0 +1,82 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import Spinner from "@/components/ui/spinner";
+import { registerExtractToSeat } from "@/lib/data";
+import { Account } from "@/modules/account/types/account";
+import useAccounts from "@/modules/shared/hooks/useAccounts";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { useState } from "react";
+import Select, { SingleValue } from "react-select";
+import { toast } from "sonner";
+
+type RegisterSeatProps = {
+  bankExtractId: number;
+  bankId: string | number;
+  hasBeenRegisteredToAccount: boolean;
+  extractAccountId: number;
+};
+
+export default function RegisterSeat({
+  bankExtractId,
+  bankId,
+  hasBeenRegisteredToAccount,
+  extractAccountId,
+}: RegisterSeatProps) {
+  const [selectedAccount, setSelectedAccount] =
+    useState<null | SingleValue<Account>>(null);
+  const queryClient = useQueryClient();
+  const { data: accounts, isPending } = useAccounts();
+
+  const registerSeatMutation = useMutation({
+    mutationFn: registerExtractToSeat,
+    onError: (error: AxiosError) => {
+      console.log(error);
+      toast.error("Hubo un error al registrar el extracto con el asiento");
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["bankExcerpt", bankId] });
+      toast.success("Datos registrados correctamente");
+    },
+  });
+
+  function handleRegister() {
+    if (selectedAccount) {
+      const values = {
+        bankExtractId,
+        accountId: selectedAccount.id,
+      };
+      registerSeatMutation.mutate(values);
+    }
+  }
+
+  if (isPending) {
+    return <Spinner />;
+  }
+
+  return (
+    <div className="flex items-center justify-between gap-2">
+      <Select
+        isDisabled={hasBeenRegisteredToAccount}
+        onChange={(option) => {
+          setSelectedAccount(option);
+        }}
+        value={
+          selectedAccount ?? extractAccountId !== 0
+            ? accounts?.find((item) => item.id === extractAccountId)
+            : null
+        }
+        options={accounts}
+        getOptionValue={(account) => account.id.toString()}
+        getOptionLabel={(account) => `${account.code}-${account.description}`}
+      />
+      <Button
+        disabled={hasBeenRegisteredToAccount}
+        title="Registrar Asiento Contable"
+        onClick={handleRegister}
+      >
+        Registrar
+      </Button>
+    </div>
+  );
+}
