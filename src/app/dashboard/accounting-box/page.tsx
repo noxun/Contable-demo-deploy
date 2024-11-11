8ffb1e430f2478d652c70b96f00ec688bd03aa13
David Antezana, rats4final <rats4final@gmail.com>
2024-11-11 13:35:23 -0400

    feat(accounting-box-page): Agregada logica para obtener los items de una caja mediante un select

--- src/app/dashboard/accounting-box/page.tsx
+++ src/app/dashboard/accounting-box/page.tsx
@@ -1,28 +1,62 @@
 "use client";
 
-import React from "react";
-import { useQuery } from "@tanstack/react-query";
-import { fetchAccountingBox } from "@/lib/data";
+import React, { useState } from "react";
+import Select from "react-select"; // Asegúrate de tener react-select instalado
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { fetchAccountingBoxItemsById } from "@/lib/data";
 import { DataTable } from "@/components/ui/data-table";
 import { columns } from "@/modules/accounting-box/components/columns";
+import NewAccountingBoxDialog from "@/modules/accounting-box/components/NewAccountingBoxDialog";
+import useAccountingBox from "@/modules/shared/hooks/useAccountingBox";
 
 const AccountingBoxPage = () => {
+  const [accountingBoxId, setAccountingBoxId] = useState<number | null>(null);
+
+  const { data: accountingBoxList, isPending: isPendingAccountingBoxList } =
+    useAccountingBox();
+
   const {
     data: accountingBox,
     isPending,
     error,
   } = useQuery({
-    queryKey: ["accountingBox"],
-    queryFn: fetchAccountingBox,
+    queryKey: ["accountingBox", accountingBoxId],
+    queryFn: () => fetchAccountingBoxItemsById(accountingBoxId!),
+    enabled: !!accountingBoxId, // Solo ejecuta la query si hay un ID seleccionado
+    placeholderData: keepPreviousData
   });
 
-  if (isPending) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
+
+  const accountingBoxOptions =
+    accountingBoxList?.map((box) => ({
+      value: box.id,
+      label: box.name,
+    })) || [];
 
   return (
     <div>
       <h1 className="text-2xl font-bold">Caja</h1>
-      <DataTable data={accountingBox} columns={columns} />
+      <NewAccountingBoxDialog />
+
+      {isPendingAccountingBoxList ? (
+        <div>Cargando cajas...</div>
+      ) : (
+        <Select
+          options={accountingBoxOptions}
+          onChange={(selectedOption) =>
+            setAccountingBoxId(selectedOption?.value || null)
+          }
+          isClearable
+          placeholder="Selecciona una caja"
+        />
+      )}
+
+      {isPending ? (
+        <div>Cargando datos de la caja...</div>
+      ) : (
+        <DataTable data={accountingBox} columns={columns} />
+      )}
     </div>
   );
 };
