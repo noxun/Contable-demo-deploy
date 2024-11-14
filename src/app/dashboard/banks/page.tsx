f1065aa1015d18b0fbcbe307f92555e6f73950a8
David Antezana, rats4final <rats4final@gmail.com>
2024-11-14 12:11:15 -0400

    refactor(banks): Refactorizada la pagina de visualizacion de bancos
    Modularizada la parte de la tabla de los bancos

--- src/app/dashboard/banks/page.tsx
+++ src/app/dashboard/banks/page.tsx
@@ -1,48 +1,31 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
-import axios from "axios";
 import Link from "next/link";
 import { TableBank } from "@/modules/banks/components/TableBank";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
-// import { IIncomeResponse } from "@/modules/income/interface/income";
-import { IBank } from "@/modules/banks/interface/banks";
-import useToken from "@/modules/shared/hooks/useToken";
+import { useQuery } from "@tanstack/react-query";
+import { fetchAllBanks } from "@/lib/data";
 
-function Users() {
-  const { token, isTokenReady } = useToken();
-  const { data, isLoading, isPending, error } = useQuery({
+export default function BanksPage() {
+  const { data, isPending } = useQuery({
     queryKey: ["Bank"],
-    queryFn: async (): Promise<{ data: IBank[] }> =>
-      await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/Bank`, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      }),
-
-    enabled: isTokenReady,
-    staleTime: 1000 * 60 * 10,
+    queryFn: fetchAllBanks,
   });
 
-  if (isLoading || isPending) return "Loading...";
-
-  console.log("ESDATA,", data);
   return (
     <div className="flex flex-col gap-4 w-full">
       <div className="w-full flex flex-col items-center">
         <h1 className="font-bold text-3xl">BANCOS</h1>
         <div className="flex w-full items-center gap-4 justify-end">
-          <Link href="/files/example_extract_bank.xlsx" download><Button>Descargar Plantilla Extracto</Button></Link>
+          <Link href="/files/example_extract_bank.xlsx" download>
+            <Button>Descargar Plantilla Extracto</Button>
+          </Link>
           <Link href="/dashboard/banks/new">
             <Button>Nuevo Banco</Button>
           </Link>
         </div>
       </div>
-      <TableBank data={data?.data! ?? []} />
+      {isPending ? <div>Cargando...</div> : <TableBank data={data ?? []} />}
     </div>
   );
 }
-
-export default Users;
