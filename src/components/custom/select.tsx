775c1ff04615d06fa8f0b7dfebf9bba58ac733eb
David Antezana, rats4final <rats4final@gmail.com>
2024-11-21 11:31:44 -0400

    wip: select reusable con modo oscuro

---
+++ src/components/custom/select.tsx
@@ -0,0 +1,12 @@
+import { GroupBase, Props } from "react-select";
+import Select from "react-select";
+
+export default function CustomSelect<
+  Option,
+  IsMulti extends boolean = false,
+  Group extends GroupBase<Option> = GroupBase<Option>
+>(props: Props<Option, IsMulti, Group>) {
+  return (
+    <Select {...props} theme={(theme) => ({ ...theme, borderRadius: 0 })} />
+  );
+}
