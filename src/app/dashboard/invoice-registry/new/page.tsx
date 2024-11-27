ba42941441a3c8084e546a11dec1f7a82229ef7c
David Antezana, rats4final <rats4final@gmail.com>
2024-11-27 03:20:58 -0400

    feat(invoice-registry): Agregadas las paginas para ver el listado de facturas y crear un nuevo registro

---
+++ src/app/dashboard/invoice-registry/new/page.tsx
@@ -0,0 +1,5 @@
+import InvoiceRegistryForm from "@/modules/invoice-registry/components/FormNewInvoiceRegistry";
+
+export default function NewInvoiceRegistryPage() {
+  return <InvoiceRegistryForm />;
+}
--- src/app/dashboard/invoice-registry/page.tsx
+++ src/app/dashboard/invoice-registry/page.tsx
@@ -1,5 +1,14 @@
+import { Button } from "@/components/ui/button";
+import ListInvoiceRegistry from "@/modules/invoice-registry/components/ListInvoiceRegistry";
+import Link from "next/link";
+
 export default function InvoiceRegistryPage() {
   return (
-    <div>InvoiceRegistryPage</div>
+    <main className="overflow-x-visible">
+      <Button asChild>
+        <Link href="/dashboard/invoice-registry/new">Nueva Registro</Link>
+      </Button>
+      <ListInvoiceRegistry/>
+    </main>
   )
 }
