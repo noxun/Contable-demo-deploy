9a4a8daac4227192229ef16d7152a8adb1374d70
David Antezana, rats4final <rats4final@gmail.com>
2024-12-02 03:04:09 -0400

    feat(model-seats): Completada la funcionalidad para borrar asientos modelo

--- src/modules/model-seats/components/DeleteModelSeatDialog.tsx
+++ src/modules/model-seats/components/DeleteModelSeatDialog.tsx
@@ -11,31 +11,47 @@ import {
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
+import { deleteModelSeat } from "@/lib/data";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { AxiosError } from "axios";
 import { Trash } from "lucide-react";
+import { toast } from "sonner";
 
-export default function DeleteModelSeatDialog() {
+export default function DeleteModelSeatDialog({
+  modelSeatId,
+}: {
+  modelSeatId: number;
+}) {
+  const queryClient = useQueryClient();
 
   function handleClick() {
-    
+    deleteModelSeatMutation.mutate(modelSeatId);
   }
 
-  // WIP 
-  // const deleteModelSeatMutation = useMutation({
-  //   mutationFn: async () => {}
-  // })
+  const deleteModelSeatMutation = useMutation({
+    mutationFn: deleteModelSeat,
+    onError: (error: AxiosError) => {
+      console.log(error);
+      toast.error("Hubo un error al eliminar el asiento modelo");
+    },
+    onSuccess: () => {
+      toast.success("Asiento Modelo borrado correctamente");
+      queryClient.invalidateQueries({ queryKey: ["AllModelSeats"] });
+    },
+  });
 
   return (
     <AlertDialog>
       <AlertDialogTrigger asChild>
-        <Button variant="outline" size="icon"> 
-          <Trash className="size-4"/>
+        <Button variant="outline" size="icon">
+          <Trash className="size-4" />
         </Button>
       </AlertDialogTrigger>
       <AlertDialogContent>
         <AlertDialogHeader>
           <AlertDialogTitle>Esta Seguro?</AlertDialogTitle>
           <AlertDialogDescription>
-            Esta Accion borrara el modelo seleccionado y sus elementos
+            Esta acción borrara el modelo seleccionado y sus elementos
           </AlertDialogDescription>
         </AlertDialogHeader>
         <AlertDialogFooter>
