c12c4f4e4b12496eaeb183b38702c9af508bcc53
David Antezana, rats4final <rats4final@gmail.com>
2024-12-13 01:00:33 -0400

    feat(vouchers): Agregados props a componentes relacionados con la edicion de vouchers
    Arreglado error en el que la queryKey era diferente, variando en string o number
     Agregado prop opcional accountDate para invalidar la query del libro mayor cuando se llama al componente EditVoucher
     Serializada la parte de accountDate en la queryKey del libro mayor

--- src/modules/shared/components/EditVoucher.tsx
+++ src/modules/shared/components/EditVoucher.tsx
@@ -1,22 +1,22 @@
-"use client"
+"use client";
 import { useQuery } from "@tanstack/react-query";
 import { Voucher, VoucherType } from "../types/sharedTypes";
 import axios from "axios";
 import FormEditVoucher from "./FormEditVoucher";
 import useToken from "../hooks/useToken";
-
+import { DateRange } from "react-day-picker";
 
 type EditVoucherProps = {
-  id: string
-  type: VoucherType
-}
+  id: string;
+  type: VoucherType;
+  accountDate?: string; //para invalidar la query
+};
 
-export default function EditVoucher({id,type}: EditVoucherProps) {
-
-  const {token, isTokenReady} = useToken();
+export default function EditVoucher({ id, type, accountDate }: EditVoucherProps) {
+  const { token, isTokenReady } = useToken();
 
   const editVoucherQuery = useQuery({
-    queryKey: ["Vouchers", id, type],
+    queryKey: ["Vouchers", id.toString(), type.toString()],
     queryFn: async function (): Promise<Voucher> {
       const response = await axios.get(
         `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/Voucher?id=${id}&type=${type}`,
@@ -30,16 +30,17 @@ export default function EditVoucher({id,type}: EditVoucherProps) {
       return response.data;
     },
     enabled: isTokenReady,
-    staleTime: 1000*30*10,
-  })
+    staleTime: 1000 * 30 * 10,
+  });
 
   console.log(editVoucherQuery.data);
 
-  if(editVoucherQuery.isLoading || editVoucherQuery.isPending) return <div>Loading..</div>
+  if (editVoucherQuery.isLoading || editVoucherQuery.isPending)
+    return <div>Loading..</div>;
 
   return (
     <div>
-      <FormEditVoucher type={type} voucher={editVoucherQuery!.data!}/>
+      <FormEditVoucher type={type} voucher={editVoucherQuery!.data!} accountDate={accountDate}/>
     </div>
-  )
+  );
 }
