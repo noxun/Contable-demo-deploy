12849fb9ec44f3fc20a0d5a856917ffb808cae92
David Antezana, rats4final <rats4final@gmail.com>
2024-12-17 12:25:45 -0400

    feat(model-seats): Agregado hook para obtener los asientos modelo por tipo
    Por tipo de transaccion

---
+++ src/modules/shared/hooks/useModelSeatsByType.ts
@@ -0,0 +1,10 @@
+import { fetchAllModelSeats } from "@/lib/data";
+import { useQuery } from "@tanstack/react-query";
+
+export default function useModelSeatsByType(type: number | undefined) {
+  return useQuery({
+    queryKey: ["AllModelSeats", type],
+    queryFn: () => fetchAllModelSeats(type),
+    enabled: type !== undefined ? true : false
+  });
+}
