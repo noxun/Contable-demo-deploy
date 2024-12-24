e262a5dfd76f6a87390f3d056e72db04d3f4c394
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2024-12-24 16:47:02 -0400

    refactor: Refactor DateSelector component for handling date ranges

---
+++ src/modules/shared/components/DateSelector.tsx
@@ -0,0 +1,113 @@
+import { useEffect, useState } from "react";
+import { Calendar as CalendarIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import { Label } from "@/components/ui/label";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { DateRange } from "react-day-picker";
+
+type DateSelectorProps = {
+  onDateChange: (initDate: Date | null, endDate: Date | null) => void;
+};
+
+export const DateSelector: React.FC<DateSelectorProps> = ({ onDateChange }) => {
+  const [date, setDate] = useState<DateRange | undefined>(undefined); // DateRange en lugar de un objeto con from y to
+  const [currentDay, setCurrentDay] = useState(false);
+  const [selectedMonth, setSelectedMonth] = useState<string | null>(null);
+
+  const handleDateChange = () => {
+    if (currentDay) {
+      const today = new Date();
+      onDateChange(today, today);
+    } else if (selectedMonth) {
+      const [year, month] = selectedMonth.split("-").map(Number);
+      const initDate = new Date(year, month - 1, 1);
+      const endDate = new Date(year, month, 0);
+      onDateChange(initDate, endDate);
+    } else if (date?.from && date?.to) {
+      onDateChange(date.from, date.to);
+    } else {
+      onDateChange(null, null);
+    }
+  };
+
+  useEffect(() => {
+    //se ejecuta cuando cambian las fechas(dia, mes o rango de fechas)
+    handleDateChange();
+  }, [date, currentDay, selectedMonth]);
+
+  return (
+    <>
+      <div className="space-y-2">
+        <div className="flex items-center gap-4">
+          {/* seleccionar rango de fechas */}
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button
+                disabled={currentDay || !!selectedMonth}
+                id="date"
+                variant={"outline"}
+                className={cn(
+                  "w-full justify-start text-left font-normal",
+                  !date && "text-muted-foreground"
+                )}
+              >
+                <CalendarIcon className="mr-2 h-4 w-4" />
+                {date?.from ? (
+                  date.to ? (
+                    `${date.from.toLocaleDateString()} - ${date.to.toLocaleDateString()}`
+                  ) : (
+                    date.from.toLocaleDateString()
+                  )
+                ) : (
+                  "Seleccionar fecha"
+                )}
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0" align="start">
+              <Calendar
+                initialFocus
+                mode="range"
+                selected={date}
+                onSelect={(range: DateRange | undefined) => setDate(range)} // Aseguramos que range sea DateRange o undefined
+                numberOfMonths={2}
+              />
+            </PopoverContent>
+          </Popover>
+          {/* seleccionar un mes */}
+          <Label>
+            Seleccionar Mes
+            <Input
+              type="month"
+              value={selectedMonth || ""}
+              onChange={(e) => {
+                setSelectedMonth(e.target.value);
+                setCurrentDay(false);
+                setDate(undefined);
+              }}
+              disabled={currentDay}
+            />
+          </Label>
+          {/* seleccionar un dia */}
+          <Label>
+            Día Actual
+            <Checkbox
+              checked={currentDay}
+              onCheckedChange={(checked) => {
+                setCurrentDay(checked === true);
+                setSelectedMonth(null);
+                setDate(undefined);
+              }}
+            />
+          </Label>
+
+        </div>
+      </div>
+
+
+    </>
+  );
+};
