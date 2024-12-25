3a61abc596673acee3f0d5331a7b0f706246f4ba
David Antezana, rats4final <rats4final@gmail.com>
2024-12-24 20:37:38 -0400

    feat(login): Agregado un nuevo fondo cool al login

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,20 +1,15 @@
 import LoginForm from "@/modules/auth/components/LoginForm";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function LoginPage() {
   return (
-    <main className="flex min-h-screen w-full items-center justify-center">
+    <main className="flex min-h-screen w-full items-center justify-center bg-cover bg-center bg-[url('/cool-background.svg')]">
       <Card className="w-auto">
         <CardHeader className="text-center">
           <CardTitle>Iniciar Sesión</CardTitle>
         </CardHeader>
         <CardContent>
-          <LoginForm/>
+          <LoginForm />
         </CardContent>
       </Card>
     </main>
