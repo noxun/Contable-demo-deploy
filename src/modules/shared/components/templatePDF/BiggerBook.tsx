adf039985225c16774b0a313e6fe20fd12e65669
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2024-12-27 12:50:45 -0400

    fix: add incrementing balance variable in BiggerBookTemplate

--- src/modules/shared/components/templatePDF/BiggerBook.tsx
+++ src/modules/shared/components/templatePDF/BiggerBook.tsx
@@ -68,53 +68,60 @@ export const BiggerBookTemplate: React.FC<BiggerBookTemplateProps> = ({ dateRang
   return (
     <Document>
       {
-        records.map((record, index) => (
-          <Page key={index} size={"LETTER"} style={styles.page} wrap>
-            <Text style={styles.titlePage}>
-              Libro Mayor
-            </Text>
-            <View style={[{ paddingBottom: 5 }]}>
-              <Text style={[styles.thCell, { textAlign: "center" }]}>(Expresado en {moneyType})</Text>
-              <Text style={[styles.thCell, { textAlign: "center" }]}>Fecha y Hora: {format(Date.now(), FORMAT_DATE_INITIAL)}</Text>
-            </View>
-            <View style={[{ paddingBottom: 10 }]}>
-              <Text style={[styles.thCell]}>
-                <Text style={{ fontFamily: "Helvetica-Bold" }}>Nro de Cuenta:</Text> {record.accountCode}
+        records.map((record, index) => {
+          let saldo = 0;
+          return (
+            <Page key={index} size={"LETTER"} style={styles.page} wrap>
+              <Text style={styles.titlePage}>
+                Libro Mayor
               </Text>
-              <Text style={[styles.thCell]}>
-                <Text style={{ fontFamily: "Helvetica-Bold" }}>Nombre de Cuenta:</Text> {record.accountDescription}
-              </Text>
-            </View>
-            <View style={{ width: "100%", border: 1 }}>
-              <View style={[styles.trCell, { borderBottomWidth: 1 }]}>
-                <Text style={[styles.col10, styles.thCell, { fontFamily: "Helvetica-Bold" }]}>Asiento</Text>
-                <Text style={[styles.col15, styles.thCell, { fontFamily: "Helvetica-Bold" }]}>Fecha</Text>
-                <Text style={[styles.col30, styles.thCell, { fontFamily: "Helvetica-Bold" }]}>Glosa</Text>
-                <Text style={[styles.col15, styles.thCell, { fontFamily: "Helvetica-Bold" }]}>Debe</Text>
-                <Text style={[styles.col15, styles.thCell, { fontFamily: "Helvetica-Bold" }]}>Haber</Text>
-                <Text style={[styles.col15, styles.thCell, { fontFamily: "Helvetica-Bold" }]}>Saldo</Text>
+              <View style={[{ paddingBottom: 5 }]}>
+                <Text style={[styles.thCell, { textAlign: "center" }]}>(Expresado en {moneyType})</Text>
+                <Text style={[styles.thCell, { textAlign: "center" }]}>Fecha y Hora: {format(Date.now(), FORMAT_DATE_INITIAL)}</Text>
               </View>
-              {
-                record.voucherItems.map((item, index) => (
-                  <View style={styles.trCell} key={item.accountId}>
-                    <Text style={[styles.col10, styles.tdCell]}>{index}</Text>
-                    <Text style={[styles.col15, styles.tdCell]}>{format(item.createdAt, "dd/MM/yyyy")}</Text>
-                    <Text style={[styles.col30, styles.tdCell]}>{item.gloss}</Text>
-                    <Text style={[styles.col15, styles.tdCell]}>{formatNumber(item[debitType])}</Text>
-                    <Text style={[styles.col15, styles.tdCell]}>{formatNumber(item[assetType])}</Text>
-                    <Text style={[styles.col15, styles.tdCell]}>{formatNumber((item[debitType] - item[assetType]))}</Text>
-                  </View>
-                ))
-              }
-              <View style={[styles.trCell, { borderTopWidth: 1 }]}>
-                <Text style={[styles.thCell, styles.col55, { fontFamily: "Helvetica-Bold" }]}>Total</Text>
-                <Text style={[styles.col15, styles.tdCell, { fontFamily: "Helvetica-Bold" }]}>{formatNumber(record.totalDebit)}</Text>
-                <Text style={[styles.col15, styles.tdCell, { fontFamily: "Helvetica-Bold" }]}>{formatNumber(record.totalAsset)}</Text>
-                <Text style={[styles.col15, styles.tdCell, { fontFamily: "Helvetica-Bold" }]}>{formatNumber((record.totalDebit - record.totalAsset))}</Text>
+              <View style={[{ paddingBottom: 10 }]}>
+                <Text style={[styles.thCell]}>
+                  <Text style={{ fontFamily: "Helvetica-Bold" }}>Nro de Cuenta:</Text> {record.accountCode}
+                </Text>
+                <Text style={[styles.thCell]}>
+                  <Text style={{ fontFamily: "Helvetica-Bold" }}>Nombre de Cuenta:</Text> {record.accountDescription}
+                </Text>
               </View>
-            </View>
-          </Page>
-        ))
+              <View style={{ width: "100%", border: 1 }}>
+                <View style={[styles.trCell, { borderBottomWidth: 1 }]}>
+                  <Text style={[styles.col10, styles.thCell, { fontFamily: "Helvetica-Bold" }]}>Asiento</Text>
+                  <Text style={[styles.col15, styles.thCell, { fontFamily: "Helvetica-Bold" }]}>Fecha</Text>
+                  <Text style={[styles.col30, styles.thCell, { fontFamily: "Helvetica-Bold" }]}>Glosa</Text>
+                  <Text style={[styles.col15, styles.thCell, { fontFamily: "Helvetica-Bold" }]}>Debe</Text>
+                  <Text style={[styles.col15, styles.thCell, { fontFamily: "Helvetica-Bold" }]}>Haber</Text>
+                  <Text style={[styles.col15, styles.thCell, { fontFamily: "Helvetica-Bold" }]}>Saldo</Text>
+                </View>
+                {
+                  record.voucherItems.map((item, index) => {
+                    saldo += Number(item[debitType]) - Number(item[assetType])
+
+                    return (
+                      <View style={styles.trCell} key={item.accountId}>
+                        <Text style={[styles.col10, styles.tdCell]}>{index}</Text>
+                        <Text style={[styles.col15, styles.tdCell]}>{format(item.createdAt, "dd/MM/yyyy")}</Text>
+                        <Text style={[styles.col30, styles.tdCell]}>{item.gloss}</Text>
+                        <Text style={[styles.col15, styles.tdCell]}>{formatNumber(item[debitType])}</Text>
+                        <Text style={[styles.col15, styles.tdCell]}>{formatNumber(item[assetType])}</Text>
+                        <Text style={[styles.col15, styles.tdCell]}>{formatNumber((saldo))}</Text>
+                      </View>
+                    )
+                  })
+                }
+                <View style={[styles.trCell, { borderTopWidth: 1 }]}>
+                  <Text style={[styles.thCell, styles.col55, { fontFamily: "Helvetica-Bold" }]}>Total</Text>
+                  <Text style={[styles.col15, styles.tdCell, { fontFamily: "Helvetica-Bold" }]}>{formatNumber(record.totalDebit)}</Text>
+                  <Text style={[styles.col15, styles.tdCell, { fontFamily: "Helvetica-Bold" }]}>{formatNumber(record.totalAsset)}</Text>
+                  <Text style={[styles.col15, styles.tdCell, { fontFamily: "Helvetica-Bold" }]}>{formatNumber((record.totalDebit - record.totalAsset))}</Text>
+                </View>
+              </View>
+            </Page>
+          )
+        })
       }
     </Document >
   )
