6ab0a4423e9d79501ebb76e208a7c504209c296a
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-01-03 17:22:48 -0400

    feat: Refactor EstadoResultadosTemplate to utilize structured records and improve formatting

--- src/modules/shared/components/templatePDF/EstadoResultados.tsx
+++ src/modules/shared/components/templatePDF/EstadoResultados.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/alt-text */
 "use client"
 import { Document, Image, Page, StyleSheet, Text, View } from "@react-pdf/renderer"
+import { formatNumber } from "../../utils/validate";
 
 const styles = StyleSheet.create({
   page: {
@@ -31,13 +32,13 @@ const styles = StyleSheet.create({
     width: "100%",
   },
   thCell: {
-    fontSize: 13,
+    fontSize: 12,
     fontFamily: "Helvetica-Bold",
     padding: 3,
   },
   tdCell: {
     fontSize: 10,
-    padding: 5,
+    padding: 3,
   },
   entry: {
     flexDirection: "row",
@@ -73,6 +74,24 @@ const styles = StyleSheet.create({
   },
 });
 
+interface Item {
+  account: string,
+  description: string,
+  amount: number,
+  amountSus: number
+}
+
+interface Records {
+  totalExpense: number,
+  totalIncome: number,
+  periodUtility: number,
+  taxOnProfits: number,
+  taxOnProfitsSus: number,
+  managementResult: number,
+  expenses: Item[];
+  income: Item[];
+}
+
 interface DateRange {
   from: string;
   to: string;
@@ -80,139 +99,38 @@ interface DateRange {
 
 interface Props {
   inSus?: boolean;
-  records?: any;
+  records: Records;
   dateRange?: DateRange;
 }
 
-export const EstadoResultadosTemplate = ({ dateRange, inSus }: Props) => {
+export const EstadoResultadosTemplate = ({ records, dateRange, inSus }: Props) => {
+
   const messageDate =
     dateRange?.from === dateRange?.to
       ? dateRange?.from
       : `${dateRange?.from} al ${dateRange?.to}`;
   const moneyType = inSus ? "Dolares" : "Bolivianos";
 
-  const gastoType = inSus ? "totalSus" : "totalBs";
+  const gastoType = inSus ? "amountSus" : "amount";
+  const taxOnProfits = inSus ? "taxOnProfitsSus" : "taxOnProfits"
 
-  const mockData = {
-    ingresos: [
-      {
-        account: "1231233",
-        accountName: "Ventas",
-        totalSus: 1231,
-        totalBs: 534654
-      },
-      {
-        account: "1231234",
-        accountName: "Ingresos por Servicios",
-        totalSus: 874,
-        totalBs: 38974
-      },
-      {
-        account: "1231235",
-        accountName: "Ingresos Diversos",
-        totalSus: 543,
-        totalBs: 28976
-      },
-      {
-        account: "1231236",
-        accountName: "Ingresos por Alquileres",
-        totalSus: 320,
-        totalBs: 15000
-      },
-      {
-        account: "1231237",
-        accountName: "Ingresos por Intereses",
-        totalSus: 450,
-        totalBs: 21300
-      }
-    ],
-    gastos: [
-      {
-        account: "987234",
-        accountName: "Costo de Ventas",
-        totalSus: 9898,
-        totalBs: 98732
-      },
-      {
-        account: "987235",
-        accountName: "Sueldos y Salarios",
-        totalSus: 7500,
-        totalBs: 102300
-      },
-      {
-        account: "987236",
-        accountName: "Servicios Básicos",
-        totalSus: 1500,
-        totalBs: 10000
-      },
-      {
-        account: "987237",
-        accountName: "Mantenimiento",
-        totalSus: 2200,
-        totalBs: 15600
-      },
-      {
-        account: "987238",
-        accountName: "Publicidad",
-        totalSus: 1800,
-        totalBs: 12500
-      },
-      {
-        account: "987239",
-        accountName: "Transporte",
-        totalSus: 500,
-        totalBs: 3500
-      },
-      {
-        account: "987240",
-        accountName: "Gastos de Oficina",
-        totalSus: 1200,
-        totalBs: 8700
-      },
-      {
-        account: "987241",
-        accountName: "Capacitación",
-        totalSus: 1000,
-        totalBs: 6500
-      },
-      {
-        account: "987242",
-        accountName: "Gastos Legales",
-        totalSus: 900,
-        totalBs: 5600
-      },
-      {
-        account: "987243",
-        accountName: "Donaciones",
-        totalSus: 300,
-        totalBs: 2500
-      }
-    ],
-    totales: {
-      totalGastoBs: 2934,
-      totalGastoSus: 543,
-      totalIngresosBs: 6545,
-      totalIngresosSus: 984,
-      impuestosBs: 1298,
-      impuestosSus: 7834,
-    }
-  };
-
+  let totalIngresos = 0
+  let totalGastos = 0
 
 
   return (
     <Document>
       <Page style={styles.page} orientation="landscape">
-        {/* <View>
+        <View>
           <Image
             style={styles.imageOutOfBounds}
             src="/images/tradecruz_logo.png"
           />
-        </View> */}
+        </View>
 
         <View style={[styles.header, { paddingBottom: 10 }]}>
-          <Text style={[styles.thCell, { fontSize: 20, }]}>Balance General</Text>
-          <Text style={[styles.thCell]}>{messageDate} Por el periodo terminado de 2024</Text>
+          <Text style={[styles.thCell, { fontSize: 20, }]}>Estado de Resultados</Text>
+          <Text style={[styles.thCell]}>{messageDate}</Text>
           <Text style={[styles.thCell]}>(Expresado en {moneyType})</Text>
         </View>
 
@@ -225,12 +143,13 @@ export const EstadoResultadosTemplate = ({ dateRange, inSus }: Props) => {
             </View>
             <View style={{ textAlign: "left" }}>
               {
-                mockData.gastos.map((entry, index) => {
+                records?.expenses.map((entry, index) => {
+                  totalGastos += entry[gastoType]
                   return (
                     <View style={styles.trCell} key={index}>
                       <Text style={[styles.tdCell, styles.col20]}>{entry.account}</Text>
-                      <Text style={[styles.tdCell, styles.col60]}>{entry.accountName}</Text>
-                      <Text style={[styles.tdCell, styles.col20]}>{entry[gastoType]}</Text>
+                      <Text style={[styles.tdCell, styles.col60]}>{entry.description}</Text>
+                      <Text style={[styles.tdCell, styles.col20]}>{formatNumber(entry[gastoType])}</Text>
                     </View>
                   )
                 })
@@ -246,12 +165,13 @@ export const EstadoResultadosTemplate = ({ dateRange, inSus }: Props) => {
             </View>
             <View style={{ textAlign: "left" }}>
               {
-                mockData.ingresos.map((entry, index) => {
+                records?.income.map((entry, index) => {
+                  totalIngresos += entry[gastoType]
                   return (
                     <View style={styles.trCell} key={index}>
                       <Text style={[styles.tdCell, styles.col20]}>{entry.account}</Text>
-                      <Text style={[styles.tdCell, styles.col60]}>{entry.accountName}</Text>
-                      <Text style={[styles.tdCell, styles.col20]}>{entry[gastoType]}</Text>
+                      <Text style={[styles.tdCell, styles.col60]}>{entry.description}</Text>
+                      <Text style={[styles.tdCell, styles.col20]}>{formatNumber(entry[gastoType])}</Text>
                     </View>
                   )
                 })
@@ -264,33 +184,48 @@ export const EstadoResultadosTemplate = ({ dateRange, inSus }: Props) => {
           <View style={[styles.trCell, styles.col50]}>
             <Text style={styles.col20} />
             <Text style={[styles.thCell, styles.col60]}>Total Gastos</Text>
-            <Text style={[styles.thCell, styles.col20]}>98234</Text>
+            <Text style={[styles.thCell, styles.col20]}>{formatNumber(totalGastos)}</Text>
           </View>
           <View style={[styles.trCell, styles.col50]}>
             <Text style={styles.col20} />
             <Text style={[styles.thCell, styles.col60]}>Total Ingresos</Text>
-            <Text style={[styles.thCell, styles.col20]}>76534</Text>
+            <Text style={[styles.thCell, styles.col20]}>{formatNumber(totalIngresos)}</Text>
           </View>
         </View>
 
-        <View style={[styles.table, { paddingTop: 10, justifyContent: "flex-end" }]}>
+        <View style={[styles.table, styles.trCell, { paddingTop: 10, justifyContent: "flex-end" }]}>
+          <View style={styles.col50} />
           <View style={[styles.col50]}>
             <View style={[styles.trCell]}>
-              <Text style={[styles.thCell, styles.col60]}>Utilidad del Periodo (Antes de impuestos)</Text>
-              <Text style={[styles.thCell, styles.col20]}>76534</Text>
+              <Text style={styles.col20} />
+              <Text style={[styles.thCell, styles.col60]}>Utilidad antes de impuestos</Text>
+              <Text style={[styles.thCell, styles.col20]}>{formatNumber((totalIngresos - totalGastos))}</Text>
             </View>
             <View style={[styles.trCell]}>
+              <Text style={styles.col20} />
               <Text style={[styles.thCell, styles.col60]}>Impuestos a las Utilidades</Text>
-              <Text style={[styles.thCell, styles.col20]}>76534</Text>
+              <Text style={[styles.thCell, styles.col20]}>{formatNumber(Number(records[taxOnProfits]))}</Text>
             </View>
             <View style={[styles.trCell]}>
+              <Text style={styles.col20} />
               <Text style={[styles.thCell, styles.col60]}>Resultado de la Gestión</Text>
-              <Text style={[styles.thCell, styles.col20]}>76534</Text>
+              <Text style={[styles.thCell, styles.col20]}>
+                {formatNumber(((totalIngresos - totalGastos) - Number(records?.taxOnProfits)))}
+              </Text>
             </View>
           </View>
         </View>
 
-
+        <View style={[styles.trCell]}>
+          <View style={[styles.thCell, styles.col50, { paddingTop: '2cm', alignItems: "center" }]}>
+            <Text style={{ paddingBottom: 3 }}>___________________________</Text>
+            <Text style={{ textAlign: "center" }}>CONTADOR</Text>
+          </View>
+          <View style={[styles.thCell, styles.col50, { paddingTop: '2cm', alignItems: "center" }]}>
+            <Text style={{ paddingBottom: 3 }}>___________________________</Text>
+            <Text style={{ textAlign: "center" }}>GERENTE</Text>
+          </View>
+        </View>
 
       </Page>
     </Document>
