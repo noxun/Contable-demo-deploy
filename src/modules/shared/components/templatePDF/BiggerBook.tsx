788fd9128faf4eae025ba9f09c7e410fdf50233e
David Antezana, rats4final <rats4final@gmail.com>
2025-01-17 14:52:18 -0400

    feat(reports): Formatea y mejora un poco el codigo de BiggerBook.tsx

--- src/modules/shared/components/templatePDF/BiggerBook.tsx
+++ src/modules/shared/components/templatePDF/BiggerBook.tsx
@@ -1,27 +1,34 @@
 /* eslint-disable jsx-a11y/alt-text */
-"use client"
+"use client";
 import { AccountData } from "@/app/dashboard/results/bigger-book/page";
-import { Document, Image, Page, StyleSheet, Text, View } from "@react-pdf/renderer"
+import {
+  Document,
+  Image,
+  Page,
+  StyleSheet,
+  Text,
+  View,
+} from "@react-pdf/renderer";
 import { format } from "date-fns";
 import { DateRange } from "react-day-picker";
 import { formatNumber } from "../../utils/validate";
 
-interface BiggerBookTemplateProps {
+type BiggerBookTemplateProps = {
   dateRange?: DateRange;
   inSus: boolean;
-  records: AccountData[]
+  records: AccountData[];
 };
 
 const styles = StyleSheet.create({
   page: {
     padding: "1cm",
     width: "100%",
-    fontFamily: 'Helvetica'
+    fontFamily: "Helvetica",
   },
   titlePage: {
     fontSize: 18,
     textAlign: "center",
-    paddingBottom: 5
+    paddingBottom: 5,
   },
   trCell: {
     flexDirection: "row",
@@ -39,149 +46,323 @@ const styles = StyleSheet.create({
     padding: 5,
   },
   col10: {
-    width: "10%"
+    width: "10%",
   },
   col11: {
-    width: "11%"
+    width: "11%",
   },
   col13: {
-    width: "13%"
+    width: "13%",
   },
   col15: {
-    width: "15%"
+    width: "15%",
   },
   col20: {
-    width: "20%"
+    width: "20%",
   },
   col25: {
-    width: "25%"
+    width: "25%",
   },
   col30: {
-    width: "30%"
+    width: "30%",
   },
   col55: {
-    width: "55%"
+    width: "55%",
   },
   col65: {
-    width: "65%"
+    width: "65%",
   },
   col70: {
-    width: "70%"
+    width: "70%",
   },
   col85: {
-    width: "85%"
+    width: "85%",
   },
   col61: {
-    width: "61%"
+    width: "61%",
   },
   imageOutOfBounds: {
-    position: 'absolute',
+    position: "absolute",
     top: -15,
     left: -15,
     height: "1.5cm",
-    objectFit: 'cover',
+    objectFit: "cover",
   },
 });
 
-export const BiggerBookTemplate: React.FC<BiggerBookTemplateProps> = ({ dateRange, inSus = false, records }) => {
+export function BiggerBookTemplate({
+  dateRange,
+  inSus = false,
+  records,
+}: BiggerBookTemplateProps) {
   // console.log('los records son: ', records)
-  const FORMAT_DATE_INITIAL = 'dd/MM/yyyy';
+  const FORMAT_DATE_INITIAL = "dd/MM/yyyy";
   const debitType = inSus ? "debitSus" : "debitBs";
   const assetType = inSus ? "assetSus" : "assetBs";
   const moneyType = inSus ? "Dolares" : "Bolivianos";
   const saldoType = inSus ? "totalSaldoSus" : "totalSaldoBs";
   const saldoTextType = inSus ? "totalSaldoTextSus" : "totalSaldoText";
-  const dateText = dateRange?.from && (
-    dateRange?.to ?
-      `Del ${format(dateRange.from, FORMAT_DATE_INITIAL)} al ${format(dateRange.to, FORMAT_DATE_INITIAL)}`
-      : `Del ${format(dateRange.from, FORMAT_DATE_INITIAL)}`
-  )
+  const dateText =
+    dateRange?.from &&
+    (dateRange?.to
+      ? `Del ${format(dateRange.from, FORMAT_DATE_INITIAL)} al ${format(
+          dateRange.to,
+          FORMAT_DATE_INITIAL
+        )}`
+      : `Del ${format(dateRange.from, FORMAT_DATE_INITIAL)}`);
 
   return (
     <Document>
-      {
-        records.map((record, index) => {
-          let saldo = 0;
-          let totalDebit = 0;
-          let totalAsset = 0;
-          return (
-            <Page key={index} size={"LETTER"} style={styles.page} wrap>
-              <View>
-                <Image
-                  style={styles.imageOutOfBounds}
-                  src="/images/tradecruz_logo.png"
-                />
-              </View>
-              <Text style={[styles.titlePage, { fontFamily: "Helvetica-Bold", paddingTop: 20 }]}>
-                Libro Mayor
+      {records.map((record) => {
+        let saldo = 0;
+        let totalDebit = 0;
+        let totalAsset = 0;
+        return (
+          <Page key={record.accountCode} size={"LETTER"} style={styles.page} wrap>
+            <View>
+              <Image
+                style={styles.imageOutOfBounds}
+                src="/images/tradecruz_logo.png"
+              />
+            </View>
+            <Text
+              style={[
+                styles.titlePage,
+                { fontFamily: "Helvetica-Bold", paddingTop: 20 },
+              ]}
+            >
+              Libro Mayor
+            </Text>
+            <View style={[{ paddingBottom: 5 }]}>
+              <Text
+                style={[
+                  styles.thCell,
+                  { textAlign: "center", fontFamily: "Helvetica-Bold" },
+                ]}
+              >
+                (Expresado en {moneyType})
               </Text>
-              <View style={[{ paddingBottom: 5 }]}>
-                <Text style={[styles.thCell, { textAlign: "center", fontFamily: "Helvetica-Bold" }]}>(Expresado en {moneyType})</Text>
-                <Text style={[styles.thCell, { textAlign: "center" }]}>{dateText}</Text>
-              </View>
-              <View style={[{ paddingBottom: 10 }]}>
-                <Text style={[styles.thCell]}>
-                  <Text style={{ fontFamily: "Helvetica-Bold" }}>Nro de Cuenta:</Text> {record.accountCode}
+              <Text style={[styles.thCell, { textAlign: "center" }]}>
+                {dateText}
+              </Text>
+            </View>
+            <View style={[{ paddingBottom: 10 }]}>
+              <Text style={[styles.thCell]}>
+                <Text style={{ fontFamily: "Helvetica-Bold" }}>
+                  Nro de Cuenta:
+                </Text>{" "}
+                {record.accountCode}
+              </Text>
+              <Text style={[styles.thCell]}>
+                <Text style={{ fontFamily: "Helvetica-Bold" }}>
+                  Nombre de Cuenta:
+                </Text>{" "}
+                {record.accountDescription}
+              </Text>
+            </View>
+            <View style={{ width: "100%", border: 1 }}>
+              <View style={[styles.trCell, { borderBottomWidth: 1 }]}>
+                <Text
+                  style={[
+                    styles.col10,
+                    styles.thCell,
+                    { fontFamily: "Helvetica-Bold" },
+                  ]}
+                >
+                  ASIENTO
                 </Text>
-                <Text style={[styles.thCell]}>
-                  <Text style={{ fontFamily: "Helvetica-Bold" }}>Nombre de Cuenta:</Text> {record.accountDescription}
+                <Text
+                  style={[
+                    styles.col11,
+                    styles.thCell,
+                    { fontFamily: "Helvetica-Bold" },
+                  ]}
+                >
+                  FECHA
+                </Text>
+                <Text
+                  style={[
+                    styles.col25,
+                    styles.thCell,
+                    { fontFamily: "Helvetica-Bold" },
+                  ]}
+                >
+                  GLOSA
+                </Text>
+                <Text
+                  style={[
+                    styles.col15,
+                    styles.thCell,
+                    { fontFamily: "Helvetica-Bold" },
+                  ]}
+                >
+                  HOJA DE RUTA
+                </Text>
+                <Text
+                  style={[
+                    styles.col13,
+                    styles.thCell,
+                    { fontFamily: "Helvetica-Bold", textAlign: "center" },
+                  ]}
+                >
+                  DEBE
+                </Text>
+                <Text
+                  style={[
+                    styles.col13,
+                    styles.thCell,
+                    { fontFamily: "Helvetica-Bold", textAlign: "center" },
+                  ]}
+                >
+                  HABER
+                </Text>
+                <Text
+                  style={[
+                    styles.col13,
+                    styles.thCell,
+                    { fontFamily: "Helvetica-Bold", textAlign: "center" },
+                  ]}
+                >
+                  SALDO
                 </Text>
               </View>
-              <View style={{ width: "100%", border: 1 }}>
-                <View style={[styles.trCell, { borderBottomWidth: 1 }]}>
-                  <Text style={[styles.col10, styles.thCell, { fontFamily: "Helvetica-Bold" }]}>ASIENTO</Text>
-                  <Text style={[styles.col11, styles.thCell, { fontFamily: "Helvetica-Bold" }]}>FECHA</Text>
-                  <Text style={[styles.col25, styles.thCell, { fontFamily: "Helvetica-Bold" }]}>GLOSA</Text>
-                  <Text style={[styles.col15, styles.thCell, { fontFamily: "Helvetica-Bold" }]}>HOJA DE RUTA</Text>
-                  <Text style={[styles.col13, styles.thCell, { fontFamily: "Helvetica-Bold", textAlign: "center" }]}>DEBE</Text>
-                  <Text style={[styles.col13, styles.thCell, { fontFamily: "Helvetica-Bold", textAlign: "center" }]}>HABER</Text>
-                  <Text style={[styles.col13, styles.thCell, { fontFamily: "Helvetica-Bold", textAlign: "center" }]}>SALDO</Text>
-                </View>
+              {record.voucherItems.map((item, index) => {
+                saldo = Number(item[saldoType]);
+                totalDebit += Number(item[debitType]);
+                totalAsset += Number(item[assetType]);
+
+                return (
+                  <View style={styles.trCell} key={item.accountId}>
+                    <Text style={[styles.col10, styles.tdCell]}>
+                      {item?.typeDes ?? index}
+                    </Text>
+                    <Text style={[styles.col11, styles.tdCell]}>
+                      {format(item.createdAt, "dd/MM/yyyy")}
+                    </Text>
+                    <Text style={[styles.col25, styles.tdCell]}>
+                      {item.gloss}
+                    </Text>
+                    <Text style={[styles.col15, styles.tdCell]}>
+                      {item.hojaDeRuta ?? " "}
+                    </Text>
+                    <Text
+                      style={[
+                        styles.col13,
+                        styles.tdCell,
+                        { textAlign: "right" },
+                      ]}
+                    >
+                      {formatNumber(item[debitType])}
+                    </Text>
+                    <Text
+                      style={[
+                        styles.col13,
+                        styles.tdCell,
+                        { textAlign: "right" },
+                      ]}
+                    >
+                      {formatNumber(item[assetType])}
+                    </Text>
+                    <Text
+                      style={[
+                        styles.col13,
+                        styles.tdCell,
+                        { textAlign: "right" },
+                      ]}
+                    >
+                      {formatNumber(item[saldoType])}
+                    </Text>
+                  </View>
+                );
+              })}
+              <View style={[styles.trCell, { borderTopWidth: 1 }]}>
+                <Text
+                  style={[
+                    styles.thCell,
+                    styles.col61,
+                    { fontFamily: "Helvetica-Bold" },
+                  ]}
+                >
+                  Total
+                </Text>
+                <Text
+                  style={[
+                    styles.col13,
+                    styles.tdCell,
+                    { fontFamily: "Helvetica-Bold", textAlign: "right" },
+                  ]}
+                >
+                  {formatNumber(totalDebit)}
+                </Text>
+                <Text
+                  style={[
+                    styles.col13,
+                    styles.tdCell,
+                    { fontFamily: "Helvetica-Bold", textAlign: "right" },
+                  ]}
+                >
+                  {formatNumber(totalAsset)}
+                </Text>
+                <Text
+                  style={[
+                    styles.col13,
+                    styles.tdCell,
+                    { fontFamily: "Helvetica-Bold", textAlign: "right" },
+                  ]}
+                >
+                  {formatNumber(saldo)}
+                </Text>
+              </View>
+              <View style={[styles.trCell, { borderTopWidth: 1 }]}>
+                <Text style={[styles.tdCell, { fontFamily: "Helvetica-Bold" }]}>
+                  SALDO {record[saldoTextType]}
+                </Text>
+              </View>
+            </View>
+
+            <View
+              style={[
+                styles.trCell,
                 {
-                  record.voucherItems.map((item, index) => {
-                    saldo = Number(item[saldoType])
-                    totalDebit += Number(item[debitType])
-                    totalAsset += Number(item[assetType])
-
-                    return (
-                      <View style={styles.trCell} key={item.accountId}>
-                        <Text style={[styles.col10, styles.tdCell]}>{item?.typeDes ?? index}</Text>
-                        <Text style={[styles.col11, styles.tdCell]}>{format(item.createdAt, "dd/MM/yyyy")}</Text>
-                        <Text style={[styles.col25, styles.tdCell]}>{item.gloss}</Text>
-                        <Text style={[styles.col15, styles.tdCell]}>{item.hojaDeRuta ?? ' '}</Text>
-                        <Text style={[styles.col13, styles.tdCell, { textAlign: "right" }]}>{formatNumber(item[debitType])}</Text>
-                        <Text style={[styles.col13, styles.tdCell, { textAlign: "right" }]}>{formatNumber(item[assetType])}</Text>
-                        <Text style={[styles.col13, styles.tdCell, { textAlign: "right" }]}>{formatNumber((item[saldoType]))}</Text>
-                      </View>
-                    )
-                  })
-                }
-                <View style={[styles.trCell, { borderTopWidth: 1 }]}>
-                  <Text style={[styles.thCell, styles.col61, { fontFamily: "Helvetica-Bold" }]}>Total</Text>
-                  <Text style={[styles.col13, styles.tdCell, { fontFamily: "Helvetica-Bold", textAlign: "right" }]}>{formatNumber(totalDebit)}</Text>
-                  <Text style={[styles.col13, styles.tdCell, { fontFamily: "Helvetica-Bold", textAlign: "right" }]}>{formatNumber(totalAsset)}</Text>
-                  <Text style={[styles.col13, styles.tdCell, { fontFamily: "Helvetica-Bold", textAlign: "right" }]}>{formatNumber((saldo))}</Text>
-                </View>
-                <View style={[styles.trCell, { borderTopWidth: 1 }]}>
-                  <Text style={[styles.tdCell, { fontFamily: 'Helvetica-Bold' }]} >SALDO {record[saldoTextType]}</Text>
-                </View>
+                  borderBottomWidth: 1,
+                  borderLeftWidth: 1,
+                  borderRightWidth: 1,
+                },
+              ]}
+            >
+              <View style={styles.col25}></View>
+              <View
+                style={[
+                  styles.thCell,
+                  styles.col25,
+                  {
+                    paddingTop: "2cm",
+                    alignItems: "center",
+                    borderLeftWidth: 1,
+                    borderRightWidth: 1,
+                  },
+                ]}
+              >
+                <Text style={{ textAlign: "center" }}>CONTADOR</Text>
               </View>
-
-              <View style={[styles.trCell, { borderBottomWidth: 1, borderLeftWidth: 1, borderRightWidth: 1 }]}>
-                <View style={styles.col25}></View>
-                <View style={[styles.thCell, styles.col25, { paddingTop: '2cm', alignItems: "center", borderLeftWidth: 1, borderRightWidth: 1 }]}>
-                  <Text style={{ textAlign: "center" }}>CONTADOR</Text>
-                </View>
-                <View style={[styles.thCell, styles.col25, { paddingTop: '2cm', alignItems: "center", borderRightWidth: 1 }]}>
-                  <Text style={{ textAlign: "center" }}>GERENTE</Text>
-                </View>
-                <View style={styles.col25}></View>
+              <View
+                style={[
+                  styles.thCell,
+                  styles.col25,
+                  {
+                    paddingTop: "2cm",
+                    alignItems: "center",
+                    borderRightWidth: 1,
+                  },
+                ]}
+              >
+                <Text style={{ textAlign: "center" }}>GERENTE</Text>
               </View>
-
-            </Page>
-          )
-        })
-      }
-    </Document >
-  )
+              <View style={styles.col25}></View>
+            </View>
+          </Page>
+        );
+      })}
+    </Document>
+  );
 }
