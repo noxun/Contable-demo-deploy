1b08876b13a5eb6fbd82e299d69a4169221c571f
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-01-20 10:16:59 -0400

    feat: Extend FixedAsset types with detailed properties for asset management

--- src/modules/fixed-assets/types/types.d.ts
+++ src/modules/fixed-assets/types/types.d.ts
@@ -1,3 +1,41 @@
 import { schemaFormFixedAssets } from "../schemas/schema";
 
 export type SchemaFixedAsset = z.infer<typeof schemaFormFixedAssets>
+
+export interface FixedAssetsAll {
+  totalIncreiceUpdate: number;
+  totalDActual: number;
+  totalIncreiceUpdateDP: number;
+  getFixed: FixedAsset[];
+}
+
+export interface FixedAsset {
+  fixedAssetsId?: number;
+  id: number;
+  typeActive: string;
+  detail: string;
+  reference: string;
+  proveedor: string;
+  invoice: string;
+  registrationDate: Date;
+  lifeMonths: number;
+  timeMonths: number;
+  depreciationPorcentage: number;
+  startDate: Date;
+  initialUFV: number;
+  lastDate: Date;
+  endUFV: number;
+  initialNetWorth: number;
+  worthUpdated: number;
+  increaseUpdated: number;
+  updatedDA: number;
+  previousDA: number;
+  increaseUpdatedDA: number;
+  currentDepreciation: number;
+  endNetWorth: number;
+  activePropertiesValue: number;
+  glossValue: number;
+  assetDepreciation: number;
+  fixedAseetsNetValue: number;
+  currentDepreciationP: number;
+}
