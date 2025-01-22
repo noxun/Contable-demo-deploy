bf90423cc93a7629ef7245381caa7eaf2a63a965
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-01-22 00:08:50 -0400

    feat: Integrar SalaryDialogEdit y DataTableCustom en SalariesPage con las nuevas funcionalidades

--- src/app/dashboard/salaries-payrolls/[id]/salaries/page.tsx
+++ src/app/dashboard/salaries-payrolls/[id]/salaries/page.tsx
@@ -1,13 +1,12 @@
 "use client"
-import { Button } from "@/components/ui/button"
-import { DataTable } from "@/components/ui/data-table"
 import { DeleteSalary, GetSalariesByPayrollId } from "@/lib/data"
 import { ConfirmDeleteDialog } from "@/modules/fixed-assets/components/ConfirmDeleteDialog"
+import { SalaryDialogEdit } from "@/modules/salaries-payrolls/components/SalaryDialogEdit"
 import { SalaryFormDialog } from "@/modules/salaries-payrolls/components/SalaryFormDialog"
+import { DataTableCustom } from "@/modules/shared/components/DataTableCustom"
 import { formatNumber } from "@/modules/shared/utils/validate"
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 import { format } from "date-fns"
-import { FilePenLineIcon, Trash2Icon } from "lucide-react"
 import { useParams } from "next/navigation"
 import { toast } from "sonner"
 
@@ -37,24 +36,39 @@ function SalariesPage() {
 
   const columnsSalaries = [
     {
-      header: "Bono por Antigüedad",
+      header: "F. Registro",
+      accessorKey: "currenDate",
+      cell: ({ row }: any) => format(new Date(row.original.currenDate), "dd-MM-yyyy"),
+      filterFn: (row: any, _column: any, filterValue: string) => {
+        const formattedDate = format(row.original.currenDate, "yyyy-MM");
+        console.log(formattedDate, filterValue)
+        return formattedDate === filterValue;
+      }
+    },
+    {
+      header: "B. Antigüedad",
       accessorKey: "bonusAntiquity",
       cell: ({ row }: any) => formatNumber(row.original.bonusAntiquity),
     },
     {
-      header: "Bono de Producción",
+      header: "B. Producción",
       accessorKey: "productionBonus",
       cell: ({ row }: any) => formatNumber(row.original.productionBonus),
     },
     {
-      header: "Minutos de Horas Extras",
+      header: "H. Extras \n (min)",
       accessorKey: "extraTimeMinutes",
       cell: ({ row }: any) => formatNumber(row.original.extraTimeMinutes),
     },
     {
-      header: "Valor por Horas Extras",
+      header: "Valor H. Extras",
       accessorKey: "valueForOvertime",
       cell: ({ row }: any) => formatNumber(row.original.valueForOvertime),
+    },
+    {
+      header: "AFP",
+      accessorKey: "afp",
+      cell: ({ row }: any) => formatNumber(row.original.afp),
     },
     {
       header: "Préstamo",
@@ -62,12 +76,12 @@ function SalariesPage() {
       cell: ({ row }: any) => formatNumber(row.original.loan),
     },
     {
-      header: "Curso de Capacitación en Excel",
+      header: "Capacitación Excel",
       accessorKey: "exelTrainingCorse",
       cell: ({ row }: any) => formatNumber(row.original.exelTrainingCorse),
     },
     {
-      header: "Liquidación de Multa de ANB",
+      header: "Multa de ANB",
       accessorKey: "anbFineSettlement",
       cell: ({ row }: any) => formatNumber(row.original.anbFineSettlement),
     },
@@ -77,26 +91,28 @@ function SalariesPage() {
       cell: ({ row }: any) => formatNumber(row.original.onAccount),
     },
     {
-      header: "Descuento por Retrasos",
+      header: "Descuentos",
       accessorKey: "dsctoShirtDelays",
       cell: ({ row }: any) => formatNumber(row.original.dsctoShirtDelays),
     },
     {
+      header: "Total Ganado",
+      accessorKey: "totalGanado",
+      sum: true,
+      cell: ({ row }: any) => formatNumber(row.original.totalGanado),
+    },
+    {
+      header: "Líquido Pagable",
+      accessorKey: "liquidPayable",
+      sum: true,
+      cell: ({ row }: any) => formatNumber(row.original.liquidPayable),
+    },
+    {
       header: "Acciones",
-      accessorKey: "",
       cell: ({ row }: any) => {
-        const assetId = row.original.id
         return (
           <div className="flex items-center justify-center gap-1">
-            <Button
-              variant="outline"
-              className="size-10 p-2 text-blue-500 rounded-full"
-              aria-label="Actualizar"
-              title="Actualizar"
-              onClick={() => { }}
-            >
-              <FilePenLineIcon />
-            </Button>
+            <SalaryDialogEdit idItem={row.original.id.toString()} />
             <ConfirmDeleteDialog
               onDelete={deleteMutation.mutate}
               id={id.toString()}
@@ -110,13 +126,12 @@ function SalariesPage() {
   ];
 
   return (
-    <section className="px-6">
-
+    <>
       {
         listSalaries && (
-          <>
-            <section className="grid grid-cols-5 items-center">
-              <fieldset className="col-span-4 border-2 rounded-lg p-2 mt-4 mb-6 flex">
+          <section >
+            <div className="grid grid-cols-5 items-center">
+              <fieldset className="col-span-5 lg:col-span-4 text-xs md:text-sm lg:text-base border-2 rounded-lg p-2 mt-4 mb-6 flex">
                 <legend>Informacion general</legend>
                 <div className="w-1/2">
                   <p><span className="font-semibold">Nombres y Apellidos: </span>{listSalaries?.nombres}</p>
@@ -130,18 +145,26 @@ function SalariesPage() {
                   <p><span className="font-semibold">Salario planilla interna: </span>{formatNumber(listSalaries?.internalPayrollSalary)} Bs</p>
                 </div>
               </fieldset>
-              <div className="flex justify-end items-center">
+              <div className="col-span-5 lg:col-span-1 flex justify-start lg:justify-end items-center">
                 <SalaryFormDialog idPayroll={id.toString()} />
               </div>
-            </section>
-            <DataTable columns={columnsSalaries} data={listSalaries?.salariesAndWagesItems} />
-          </>
+            </div>
+
+            <div className="max-w-full">
+              <DataTableCustom
+                columns={columnsSalaries}
+                data={listSalaries?.salariesAndWagesItems}
+                filter={{
+                  columnName: 'currenDate',
+                  placeholder: 'Filtrar por mes',
+                  type: 'month'
+                }}
+              />
+            </div>
+          </section>
         )
       }
-
-
-
-    </section>
+    </>
   )
 
 }
