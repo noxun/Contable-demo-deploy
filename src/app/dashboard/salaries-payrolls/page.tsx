94c071021df576515ff306fae86d3b1218d25154
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-01-23 14:24:52 -0400

    feat: Organizar el codigo y actualizar la seleccion de fecha por mes

--- src/app/dashboard/salaries-payrolls/page.tsx
+++ src/app/dashboard/salaries-payrolls/page.tsx
@@ -4,10 +4,8 @@ import { DataTable } from "@/components/ui/data-table";
 import { Label } from "@/components/ui/label";
 import { DeletePayroll, GetPayrollById, GetPayrollExcelByDate, GetPayrolls } from "@/lib/data";
 import { ConfirmDeleteDialog } from "@/modules/fixed-assets/components/ConfirmDeleteDialog";
-import { DatePicker } from "@/modules/fixed-assets/components/DatePicker";
 import { PayrollsDialogEdit } from "@/modules/salaries-payrolls/components/PayrollDialogEdit";
 import { PayrollsDialogForm } from "@/modules/salaries-payrolls/components/PayrollsDialogForm";
-import { Payroll } from "@/modules/salaries-payrolls/types/types";
 import PaySlipDialog from "@/modules/salaries-payrolls/components/PaySlipDialog";
 import { formatNumber } from "@/modules/shared/utils/validate";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
@@ -17,22 +15,18 @@ import Link from "next/link";
 import { useState } from "react";
 import { toast } from "sonner";
 import { Input } from "@/components/ui/input";
-import { se } from "date-fns/locale";
 
 function SalariesPayrollsPage() {
 
   const queryClient = useQueryClient();
+  const [selectedDate, setSelectedDate] = useState<string>(format(new Date(), 'yyyy-MM'));
 
-  const [selectedDate, setSelectedDate] = useState<Date | undefined>(undefined);
   const { data: listPayrolls } = useQuery({
     queryKey: ['AllPayrolls', selectedDate],
     queryFn: () => {
-      const dateSelected = selectedDate || new Date();
-      return GetPayrolls({ date: format(dateSelected, 'yyyy-MM') });
+      return GetPayrolls({ date: selectedDate });
     }
   })
-
-  console.log('los datos: ', listPayrolls);
 
   const { refetch: refetchExcel } = useQuery({
     queryKey: [selectedDate],
@@ -57,6 +51,30 @@ function SalariesPayrollsPage() {
       toast("Eliminando activo fijo...")
     }
   })
+
+  const onHandleGenerateExcel = async () => {
+    try {
+      toast.info("Generando archivo Excel...");
+      const { data: linkExcel } = await refetchExcel();
+      const fileUrl = linkExcel instanceof Blob ? URL.createObjectURL(linkExcel) : linkExcel;
+
+      const link = document.createElement("a");
+      link.href = fileUrl;
+      link.download = "planillas.xlsx";
+      document.body.appendChild(link);
+      link.click();
+
+      document.body.removeChild(link);
+      if (linkExcel instanceof Blob) {
+        URL.revokeObjectURL(fileUrl);
+      }
+
+      toast.success("Archivo Excel generado y descargado correctamente.");
+    } catch (error) {
+      console.error("Error al generar el archivo Excel:", error);
+      toast.error("Ocurrió un error al generar el archivo Excel. Por favor, inténtalo de nuevo.");
+    }
+  };
 
   const columnsPayrolls = [
     {
@@ -130,30 +148,6 @@ function SalariesPayrollsPage() {
     },
   ];
 
-  const onHandleGenerateExcel = async () => {
-    try {
-      toast.info("Generando archivo Excel...");
-      const { data: linkExcel } = await refetchExcel();
-      const fileUrl = linkExcel instanceof Blob ? URL.createObjectURL(linkExcel) : linkExcel;
-
-      const link = document.createElement("a");
-      link.href = fileUrl;
-      link.download = "planillas.xlsx";
-      document.body.appendChild(link);
-      link.click();
-
-      document.body.removeChild(link);
-      if (linkExcel instanceof Blob) {
-        URL.revokeObjectURL(fileUrl);
-      }
-
-      toast.success("Archivo Excel generado y descargado correctamente.");
-    } catch (error) {
-      console.error("Error al generar el archivo Excel:", error);
-      toast.error("Ocurrió un error al generar el archivo Excel. Por favor, inténtalo de nuevo.");
-    }
-  };
-
   return (
     <section className="px-6">
       <div className="w-full flex justify-between items-center">
@@ -164,16 +158,19 @@ function SalariesPayrollsPage() {
       <div className="flex items-end gap-4 mt-3 mb-6 ">
         <Label className="flex flex-col gap-3 w-fit" >
           Seleccione la fecha de inicio
-          <DatePicker
+          <Input
+            type="month"
+            onChange={(e) => {
+              setSelectedDate(e.target.value);
+            }}
             value={selectedDate}
-            onChange={(date) => setSelectedDate(date)}
           />
         </Label>
 
         <Button
           className="flex gap-2 items-center"
           onClick={onHandleGenerateExcel}>
-          <SheetIcon size="icon" />Descargar Excel
+          <SheetIcon />Descargar Excel
         </Button>
       </div>
 
@@ -202,8 +199,6 @@ function SalariesPayrollsPage() {
           </>
         )
       }
-
-
     </section >
   );
 }
