5c3633d4ff1acb1958c96768c326836a87644049
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-01-24 09:54:38 -0400

    style: Ajustar clases de estilos, para mejorar el diseño y adaptabilidad del dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -14,9 +14,9 @@ export default function Dashboard({ children }: { children: React.ReactNode }) {
   useInitializeUserStore();
 
   return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]  ">
+    <div className="grid max-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]  ">
       <div className="hidden border-r bg-muted/40 md:block border-gray-700">
-        <div className="flex h-full max-h-screen flex-col gap-2">
+        <div className="sticky flex max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 justify-between lg:h-[60px] lg:px-6 border-gray-500">
             <Link href="#" className="flex items-center gap-2 font-semibold">
               {/* <Archive className="h-6 w-6" />
@@ -30,7 +30,7 @@ export default function Dashboard({ children }: { children: React.ReactNode }) {
           </div>
         </div>
       </div>
-      <div className="flex flex-col ">
+      <div className="flex flex-col max-h-screen ">
         <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6 border-gray-500">
           <Sheet>
             <SheetTrigger asChild>
@@ -53,7 +53,7 @@ export default function Dashboard({ children }: { children: React.ReactNode }) {
             </Link>
           </Button>
         </header>
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+        <main className="overflow-auto flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
           {children}
         </main>
       </div>
