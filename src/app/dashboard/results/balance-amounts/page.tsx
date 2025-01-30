c1d46a92f788ff81bdfcb0c9ef13de2bed7ba583
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-01-30 18:42:49 -0400

    feat: Refactorizar codigo innecesario e Implementar la generacion de PDFs y una vista previa de los datos

--- src/app/dashboard/results/balance-amounts/page.tsx
+++ src/app/dashboard/results/balance-amounts/page.tsx
@@ -1,20 +1,11 @@
 "use client";
 import "@cyntler/react-doc-viewer/dist/index.css";
-import { addDays, format } from "date-fns";
-import { Calendar as CalendarIcon, FileText, Sheet } from "lucide-react";
+import { format } from "date-fns";
+import { Calendar as CalendarIcon, Sheet } from "lucide-react";
 import { DateRange } from "react-day-picker";
-
-import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { es } from "date-fns/locale";
 import { DataTable } from "@/components/ui/data-table";
 import { toast } from "sonner";
 import {
@@ -30,21 +21,18 @@ import DocViewer, {
 } from "@cyntler/react-doc-viewer";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
 import { BreadcrumbDashboard } from "@/modules/shared/components/BreadcrumDash";
+import { useQuery } from "@tanstack/react-query";
+import { getAllDataReportByType } from "@/lib/data";
+import { DateSelector } from "@/modules/shared/components/DateSelector";
+import { BalanceAmountsTemplate } from "@/modules/shared/components/templatePDF/BalanceAmounts";
+import { ButtonLinkPDF } from "@/modules/results/components/ButtonLinkPDF";
+import { formatNumber } from "@/modules/shared/utils/validate";
+import { DataTableCustom } from "@/modules/shared/components/DataTableCustom";
+
 
 export default function BalanceAmountsPage() {
-  // --- Estados del formulario ---
-  const [date, setDate] = useState<DateRange | undefined>({
-    from: new Date(2024, 11, 20),
-    to: addDays(new Date(2024, 11, 20), 20),
-  });
-  const [currentDay, setCurrentDay] = useState<boolean | "indeterminate">(
-    false
-  );
-  const [selectedMonth, setSelectedMonth] = useState<string | null>(null);
-  const [inSus, setInSus] = useState<boolean | "indeterminate">(false);
-
+  const [inSus, setInSus] = useState<boolean>(false);
   // --- Estados de los links ---
   const [excelLink, setExcelLink] = useState<string | null>(null);
   // const [pdfLink, setPdfLink] = useState<string | null>(null);
@@ -79,95 +67,101 @@ export default function BalanceAmountsPage() {
     setViewerKey((prevKey) => prevKey + 1);
   }, [docs]);
 
-  // --- Logica del componente ---
-  const handleClick = async () => {
-    let initDate: Date | null = null;
-    let endDate: Date | null = null;
+  //version modificada
+  const initialDateRange: DateRange = {
+    from: new Date(Date.now()),
+  }
+  const [dateRange, setDateRange] = useState<DateRange>(initialDateRange)
 
-    if (currentDay) {
-      const today = new Date();
-      initDate = today;
-      endDate = today;
-    } else if (selectedMonth) {
-      const [year, month] = selectedMonth.split("-").map(Number);
-      initDate = new Date(year, month - 1, 1);
-      endDate = new Date(year, month, 0);
-    } else if (date?.from && date?.to) {
-      initDate = date.from;
-      endDate = date.to;
-    }
-
-    if (initDate && endDate) {
-      setIsLoading(true);
-      setExcelLink(null);
-      // setPdfLink(null);
-      setDocs([]);
-      toast("Generando reporte...");
-      try {
-        // Generar el reporte de Excel
-        const excelResponse = await axios.get(
-          `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/Report/Xlxs/balanceDeSumas`,
-          {
-            params: {
-              InitDate: format(initDate, "yyyy/MM/dd"),
-              EndDate: format(endDate, "yyyy/MM/dd"),
-              type: "xlsx",
-              inSus: inSus,
-              businessId: 0, //cambiar esto cuando haya unidad de negocios
-            },
-            responseType: "text",
-          }
-        );
-        // Generar el reporte de PDF
-        // const pdfResponse = await axios.get(
-        //   `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/Report/BalanceAmounts`,
-        //   {
-        //     params: {
-        //       InitDate: format(date.from, "yyyy/MM/dd"),
-        //       EndDate: format(date.to, "yyyy/MM/dd"),
-        //       //type: "pdf",
-        //       inSus: inSus,
-        //       //Level: 5, //Averiguar que es level
-        //       businessId: 0 //cambiar esto cuando haya unidad de negocios
-        //     },
-        //     responseType: "text",
-        //   }
-        // );
-        const currentDate = new Date().toLocaleString();
-        // if (pdfResponse.data) {
-        //   setPdfLink(pdfResponse.data);
-        //   setDocs((prevDocs) => [...prevDocs, { uri: pdfResponse.data }]);
-        //   setGeneratedFiles((prevFiles) => [
-        //     ...prevFiles,
-        //     {
-        //       type: "PDF",
-        //       date: currentDate,
-        //       link: pdfResponse.data,
-        //     },
-        //   ]);
-        // }
-        if (excelResponse.data) {
-          setExcelLink(excelResponse.data);
-          setDocs((prevDocs) => [...prevDocs, { uri: excelResponse.data }]);
-          setGeneratedFiles((prevFiles) => [
-            ...prevFiles,
-            {
-              type: "Excel",
-              date: currentDate,
-              link: excelResponse.data,
-            },
-          ]);
-        }
-        setShowDialog(true);
-        toast.success("Reporte generado exitosamente");
-      } catch (error) {
-        console.error("Error al generar los reportes", error);
-        toast.error("Error al generar el reporte, intente nuevamente");
-      } finally {
-        setIsLoading(false);
-      }
+  const handleDateChange = (startDate: Date | null, endDate: Date | null) => {
+    if (startDate && endDate) {
+      setPdfFile(null)
+      setDateRange({
+        from: startDate,
+        to: endDate
+      })
     }
   };
+  const handleChangeIsSus = () => setInSus(!inSus)
+  //excel
+  const handleOnClickExcel = async () => {
+    setIsLoading(true);
+    setExcelLink(null);
+    setDocs([]);
+    toast("Generando reporte...");
+    try {
+      // Generar el reporte de Excel
+      if (!dateRange.from || !dateRange.to) return
+      const excelResponse = await axios.get(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/Report/Xlxs/balanceDeSumas`,
+        {
+          params: {
+            InitDate: format(dateRange.from, "yyyy/MM/dd"),
+            EndDate: format(dateRange.to, "yyyy/MM/dd"),
+            Level: 2,
+            inSus: inSus,
+          },
+          responseType: "text",
+        }
+      );
+
+      const currentDate = new Date().toLocaleString();
+
+      if (excelResponse.data) {
+        setExcelLink(excelResponse.data);
+        setDocs((prevDocs) => [...prevDocs, { uri: excelResponse.data }]);
+        setGeneratedFiles((prevFiles) => [
+          ...prevFiles,
+          {
+            type: "Excel",
+            date: currentDate,
+            link: excelResponse.data,
+          },
+        ]);
+      }
+      setShowDialog(true);
+      toast.success("Reporte generado exitosamente");
+    } catch (error) {
+      console.error("Error al generar los reportes", error);
+      toast.error("Error al generar el reporte, intente nuevamente");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+  const [pdfFile, setPdfFile] = useState<JSX.Element | null>(null)
+  const [isLoadingPDF, setIsLoadingPDF] = useState(false)
+
+  const { data: BalanceAmounts } = useQuery({
+    queryKey: ["AllBalanceAmounts", format(dateRange.from || new Date(), 'yyyy-MM-dd')],
+    queryFn: () => getAllDataReportByType({
+      iDate: format(dateRange.from || new Date(), 'yyyy-MM-dd'),
+      eDate: format(dateRange.to || new Date(), 'yyyy-MM-dd'),
+      typePath: "balanceDeSumas",
+      level: 2
+    }),
+    enabled: !!dateRange.from && !!dateRange?.to
+  })
+
+  const handleOnGeneratePDF = async () => {
+    setPdfFile(null)
+    if (!BalanceAmounts) return
+    try {
+      setIsLoadingPDF(true);
+      toast("Generando reporte...");
+      const MyDocument = (
+        <BalanceAmountsTemplate
+          dateRange={dateRange}
+          records={BalanceAmounts}
+        />
+      );
+      setPdfFile(MyDocument);
+      toast.success("Reporte generado exitosamente");
+    } catch (error) {
+      toast.error("Error al generar el reporte, intente nuevamente")
+    } finally {
+      setIsLoadingPDF(false);
+    }
+  }
 
   const columns = [
     { header: "Tipo", accessorKey: "type" },
@@ -180,6 +174,39 @@ export default function BalanceAmountsPage() {
           Descargar
         </a>
       ),
+    },
+  ];
+
+  const columnsBalanceAmounts = [
+    {
+      header: "Cuenta",
+      accessorKey: "type",
+      cell: ({ row }: any) => row.original.code
+    },
+    {
+      header: "Nombre",
+      accessorKey: "description",
+      cell: ({ row }: any) => row.original.description
+    },
+    {
+      header: "Debe",
+      accessorKey: "asset",
+      cell: ({ row }: any) => formatNumber(row.original.asset)
+    },
+    {
+      header: "Haber",
+      accessorKey: "debit",
+      cell: ({ row }: any) => formatNumber(row.original.debit)
+    },
+    {
+      header: "Deudor",
+      accessorKey: "debtor",
+      cell: ({ row }: any) => formatNumber(row.original.debtor)
+    },
+    {
+      header: "Acreedor",
+      accessorKey: "creditor",
+      cell: ({ row }: any) => formatNumber(row.original.creditor)
     },
   ];
 
@@ -201,84 +228,27 @@ export default function BalanceAmountsPage() {
           }
         ]}
       />
-      <div className="flex items-center justify-evenly">
-        <div className="space-y-2">
-          {/* Rango de fechas */}
-          <div className="flex items-center gap-4">
-            <Popover>
-              <PopoverTrigger asChild>
-                <Button
-                  disabled={currentDay as boolean || !!selectedMonth}
-                  id="date"
-                  variant={"outline"}
-                  className={cn(
-                    "w-full justify-start text-left font-normal",
-                    !date && "text-muted-foreground"
-                  )}
-                >
-                  <CalendarIcon className="mr-2 h-4 w-4" />
-                  {date?.from ? (
-                    date.to ? (
-                      <>
-                        {format(date.from, "LLL dd, y", { locale: es })} -{" "}
-                        {format(date.to, "LLL dd, y", { locale: es })}
-                      </>
-                    ) : (
-                      format(date.from, "LLL dd, y", { locale: es })
-                    )
-                  ) : (
-                    <span>Pick a date</span>
-                  )}
-                </Button>
-              </PopoverTrigger>
-              <PopoverContent className="w-auto p-0" align="start">
-                <Calendar
-                  initialFocus
-                  mode="range"
-                  locale={es}
-                  defaultMonth={date?.from}
-                  selected={date}
-                  onSelect={setDate}
-                  numberOfMonths={2}
-                />
-              </PopoverContent>
-            </Popover>
-            {/* Solo mes */}
-            <Label>
-              Seleccionar Mes
-              <Input
-                type="month"
-                value={selectedMonth || ""}
-                onChange={(e) => {
-                  setSelectedMonth(e.target.value);
-                  setCurrentDay(false);
-                  setDate(undefined);
-                }}
-                disabled={currentDay as boolean}
-              />
-            </Label>
-            {/* Dia Actual */}
-            <Label>
-              Dia Actual
-              <Checkbox
-                checked={currentDay}
-                onCheckedChange={(checked) => {
-                  setCurrentDay(checked);
-                  setSelectedMonth(null);
-                  setDate(undefined);
-                }}
-              />
-            </Label>
-          </div>
+      <div className="flex flex-col items-start justify-evenly md:flex-row md:items-center">
+        <div className="flex flex-col gap-2">
+          <DateSelector onDateChange={handleDateChange} />
           <div className="flex items-center space-x-2">
-            <Checkbox id="inSus" checked={inSus} onCheckedChange={setInSus} />
+            <Checkbox id="inSus" checked={inSus} onCheckedChange={handleChangeIsSus} />
             <Label htmlFor="inSus">Devolver el reporte en dolares?</Label>
           </div>
         </div>
-        <Button onClick={handleClick} disabled={isLoading}>
-          {isLoading ? "Generando Reporte..." : "Generar Reporte"}
-        </Button>
+        <div className="flex gap-4 py-3 flex-row justify-end lg:flex-row w-full md:flex-col md:w-auto sm:justify-start">
+          <Button onClick={handleOnClickExcel} disabled={isLoading}>
+            {isLoading ? "Generando Reporte..." : "Generar Reporte"}
+          </Button>
+          <Button
+            onClick={handleOnGeneratePDF}
+            disabled={!BalanceAmounts}
+          >
+            {isLoadingPDF ? 'Generando PDF...' : 'Generar PDF'}
+          </Button>
+        </div>
       </div>
+
       <Dialog open={showDialog} onOpenChange={setShowDialog}>
         {/* Comentado pero podria ser util si se requiere en algun momento */}
         {/* <DialogTrigger asChild>
@@ -307,8 +277,44 @@ export default function BalanceAmountsPage() {
           </div>
         </DialogContent>
       </Dialog>
+      {pdfFile && (
+        <ButtonLinkPDF
+          pdfFile={pdfFile}
+          nameFile="R_sumas_y_saldos"
+        />
+      )}
       <DataTable columns={columns} data={generatedFiles} />
-      <DocViewer
+      {
+        BalanceAmounts?.items && (
+          <>
+            <DataTableCustom
+              columns={columnsBalanceAmounts}
+              data={BalanceAmounts.items}
+              filter={{
+                columnName: "description",
+                placeholder: "Buscar por cuenta...",
+                type: "text"
+              }}
+            />
+            <div className="flex gap-4 justify-end items-center ">
+              {
+                [
+                  { label: 'Debe', value: BalanceAmounts?.asset },
+                  { label: 'Haber', value: BalanceAmounts?.debit },
+                  { label: 'Deudor', value: BalanceAmounts?.debtor },
+                  { label: 'Acreedor', value: BalanceAmounts?.creditor },
+                ].map((item, index) => (
+                  <div key={index} className="pb-6" >
+                    <p className="text-sm text-gray-600">{item.label}</p>
+                    <p className="text-lg font-bold">{formatNumber(item.value)}</p>
+                  </div>
+                ))
+              }
+            </div>
+          </>
+        )
+      }
+      {/* <DocViewer
         activeDocument={activeDocument}
         onDocumentChange={handleDocumentChange}
         key={viewerKey}
@@ -316,7 +322,7 @@ export default function BalanceAmountsPage() {
         documents={docs}
         pluginRenderers={[PDFRenderer, MSDocRenderer]}
         language="es"
-      />
+      /> */}
     </div>
   );
 }
