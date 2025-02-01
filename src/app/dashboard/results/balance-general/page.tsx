86b31cb9eb6644e13b111594943028227d23caeb
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-01-31 20:01:53 -0400

    feat: refactorizar e implementar la vista previa y generacion del enlace de descarga del pdf del balance general

--- src/app/dashboard/results/balance-general/page.tsx
+++ src/app/dashboard/results/balance-general/page.tsx
@@ -1,70 +1,30 @@
 "use client";
 
 import { addDays, format } from "date-fns";
-import { Calendar as CalendarIcon, FileText, Sheet } from "lucide-react";
 import { DateRange } from "react-day-picker";
-
-import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import { useState, useEffect } from "react";
-import axios from "axios";
-import { es } from "date-fns/locale";
-import { DataTable } from "@/components/ui/data-table";
+import React, { useState, useEffect } from "react";
 import { toast } from "sonner";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-
-} from "@/components/ui/dialog";
-import DocViewer, {
-  PDFRenderer,
-  MSDocRenderer,
-
-} from "@cyntler/react-doc-viewer";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
 import { BalanceGeneralTemplate } from "@/modules/shared/components/templatePDF/BalanceGeneral";
-import { PDFDownloadLink, PDFViewer } from "@react-pdf/renderer";
 import { DateSelector } from "@/modules/shared/components/DateSelector";
-import { ReportGeneratorFile } from "@/modules/shared/components/ReportGeneratorFile";
 import { ReportExcelGenerate } from "@/modules/shared/components/ReportExcelGenerator";
-import { ReportPaths } from "@/modules/shared/utils/validate";
+import { formatNumber, ReportPaths } from "@/modules/shared/utils/validate";
 import { BreadcrumbDashboard } from "@/modules/shared/components/BreadcrumDash";
+import { useQuery } from "@tanstack/react-query";
+import { getAllDataReportByType } from "@/lib/data";
+import { ButtonLinkPDF } from "@/modules/results/components/ButtonLinkPDF";
+import { BalanceGeneralPreview } from "@/modules/results/components/BalanceGeneralPreview";
 
 export default function BalanceGeneralPage() {
-  // --- Estados del formulario ---
-  const [date, setDate] = useState<DateRange | undefined>({
-    from: new Date(2024, 0, 20),
-    to: addDays(new Date(2024, 0, 20), 20),
-  });
+
   const [inSus, setInSus] = useState<boolean>(false);
 
-  // --- Estados de los links ---
-  const [excelLink, setExcelLink] = useState<string | null>(null);
-  // const [pdfLink, setPdfLink] = useState<string | null>(null);
-
-  // --- Estados de carga o visualizacion ---
-  const [isLoading, setIsLoading] = useState(false);
   const [showDialog, setShowDialog] = useState(false);
-
-  const [currentDay, setCurrentDay] = useState<boolean | "indeterminate">(
-    false
-  );
-  const [selectedMonth, setSelectedMonth] = useState<string | null>(null);
 
   // --- Estados de los documentos para la tabla y el visor ---
   const [docs, setDocs] = useState<{ uri: string }[]>([]);
-
 
   /*
     Estos estados controlan la llave y el documento activo actual del visor
@@ -75,114 +35,28 @@ export default function BalanceGeneralPage() {
     pero de momento este es el fix que se tiene.
    */
   const [viewerKey, setViewerKey] = useState(0);
-  const [activeDocument, setActiveDocument] = useState(docs[0]);
   const [generatedFiles, setGeneratedFiles] = useState<any[]>([]);
-
-  const handleDocumentChange = (document: any) => {
-    setActiveDocument(document);
-    setViewerKey((prevKey) => prevKey + 1);
-  };
 
   useEffect(() => {
     setViewerKey((prevKey) => prevKey + 1);
   }, [docs]);
-
-  // --- Logica del componente ---
-  const handleClick = async () => {
-    let initDate: Date | null = null;
-    let endDate: Date | null = null;
-
-    if (currentDay) {
-      const today = new Date();
-      initDate = today;
-      endDate = today;
-    } else if (selectedMonth) {
-      const [year, month] = selectedMonth.split("-").map(Number);
-      initDate = new Date(year, month - 1, 1);
-      endDate = new Date(year, month, 0);
-    } else if (date?.from && date?.to) {
-      initDate = date.from;
-      endDate = date.to;
-    }
-
-
-    if (initDate && endDate) {
-      setIsLoading(true);
-      setExcelLink(null);
-      // setPdfLink(null);
-      setDocs([]);
-      toast("Generando reporte...");
-      try {
-        // Generar el reporte de Excel
-        const excelResponse = await axios.get(
-          `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/Report/Xlxs/balanceGeneral`,
-          {
-            params: {
-              InitDate: format(initDate, "yyyy/MM/dd"),
-              EndDate: format(endDate, "yyyy/MM/dd"),
-              type: "xlsx",
-              inSus: inSus,
-              businessId: 0 //cambiar esto cuando haya unidad de negocios
-            },
-            responseType: "text",
-          }
-        );
-        // Generar el reporte de PDF
-        // const pdfResponse = await axios.get(
-        //   `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/Report/BalanceGeneral`,
-        //   {
-        //     params: {
-        //       InitDate: format(date.from, "yyyy/MM/dd"),
-        //       EndDate: format(date.to, "yyyy/MM/dd"),
-        //       //type: "pdf",
-        //       inSus: inSus,
-        //       //Level: 5, //Averiguar que es level
-        //       //businessId: 0 //cambiar esto cuando haya unidad de negocios
-        //     },
-        //     responseType: "text",
-        //   }
-        // );
-        const currentDate = new Date().toLocaleString();
-        // if (pdfResponse.data) {
-        //   setPdfLink(pdfResponse.data);
-        //   setDocs((prevDocs) => [...prevDocs, { uri: pdfResponse.data }]);
-        //   setGeneratedFiles((prevFiles) => [
-        //     ...prevFiles,
-        //     {
-        //       type: "PDF",
-        //       date: currentDate,
-        //       link: pdfResponse.data,
-        //     },
-        //   ]);
-        // }
-        if (excelResponse.data) {
-          setExcelLink(excelResponse.data);
-          setDocs((prevDocs) => [...prevDocs, { uri: excelResponse.data }]);
-          setGeneratedFiles((prevFiles) => [
-            ...prevFiles,
-            {
-              type: "Excel",
-              date: currentDate,
-              link: excelResponse.data,
-            },
-          ]);
-        }
-        setShowDialog(true);
-        toast.success("Reporte generado exitosamente");
-      } catch (error) {
-        console.error("Error al generar los reportes", error);
-        toast.error("Error al generar el reporte, intente nuevamente");
-      } finally {
-        setIsLoading(false);
-      }
-    }
-  };
 
   const initialDateRange: DateRange = {
     from: new Date(Date.now()),
   }
   const [pdfFile, setPdfFile] = useState<JSX.Element | null>(null);
   const [dateRange, setDateRange] = useState<DateRange>(initialDateRange);
+  const [isLoadingPDF, setIsLoadingPDF] = useState(false)
+  const { data: dataBalanceGeneral } = useQuery({
+    queryKey: ["AllBalanceGeneral", format(dateRange.from ?? new Date(), 'dd-MM-yyyy')],
+    queryFn: () => getAllDataReportByType({
+      iDate: format(dateRange.from || new Date(), 'yyyy-MM-dd'),
+      eDate: format(dateRange.to || new Date(), 'yyyy-MM-dd'),
+      typePath: "balanceGeneral",
+      level: 2
+    }),
+    enabled: !!dateRange.from && !!dateRange.to
+  })
 
   const handleOnDateChange = (startDate: Date | null, endDate: Date | null) => {
     if (startDate && endDate) {
@@ -196,93 +70,96 @@ export default function BalanceGeneralPage() {
 
   const handleChangeIsSus = () => setInSus(!inSus)
 
+  const handleOnGeneratePDF = async () => {
+    setPdfFile(null)
+    if (!dataBalanceGeneral) return
+    try {
+      setIsLoadingPDF(true);
+      toast("Generando reporte...");
+      const MyDocument = (
+        <BalanceGeneralTemplate
+          dateRange={dateRange}
+          records={dataBalanceGeneral}
+        />
+      );
+      setPdfFile(MyDocument);
+      toast.success("Reporte generado exitosamente");
+    } catch (error) {
+      toast.error("Error al generar el reporte, intente nuevamente")
+    } finally {
+      setIsLoadingPDF(false);
+    }
+  }
+
   return (
-    <div className="flex flex-col gap-6 h-full">
-      <BreadcrumbDashboard
-        items={[
-          {
-            label: "Panel",
-            href: "/dashboard"
-          },
-          {
-            label: "Reportes",
-            href: "#"
-          },
-          {
-            label: "Balance General",
-            href: "/dashboard/results/balance-general"
-          }
-        ]}
-      />
-      <div className="flex flex-col items-start justify-evenly md:flex-row md:items-center">
-        {/* Rango de fechas */}
-        <div className="flex gap-2 flex-col">
-          <DateSelector onDateChange={handleOnDateChange} />
-          <div className="flex items-center space-x-2">
-            <Checkbox id="inSus" checked={inSus} onCheckedChange={handleChangeIsSus} />
-            <Label htmlFor="inSus">Devolver el reporte en dolares?</Label>
+    <>
+      <div className="flex flex-col gap-6 h-full">
+        <BreadcrumbDashboard
+          items={[
+            {
+              label: "Panel",
+              href: "/dashboard"
+            },
+            {
+              label: "Reportes",
+              href: "#"
+            },
+            {
+              label: "Balance General",
+              href: "/dashboard/results/balance-general"
+            }
+          ]}
+        />
+        <div className="flex flex-col items-start justify-evenly md:flex-row md:items-center">
+          {/* Rango de fechas */}
+          <div className="flex gap-2 flex-col">
+            <DateSelector onDateChange={handleOnDateChange} />
+            <div className="flex items-center space-x-2">
+              <Checkbox id="inSus" checked={inSus} onCheckedChange={handleChangeIsSus} />
+              <Label htmlFor="inSus">Devolver el reporte en dolares?</Label>
+            </div>
+          </div>
+          {/* aqui generar el reporte */}
+          <div className="flex gap-4 py-3 flex-row justify-end lg:flex-row w-full md:flex-col md:w-auto sm:justify-start">
+            <Button
+              onClick={handleOnGeneratePDF}
+              disabled={!dataBalanceGeneral}
+            >
+              {isLoadingPDF ? 'Generando PDF...' : 'Generar PDF'}
+            </Button>
+            <ReportExcelGenerate
+              dateRange={dateRange}
+              inSus={inSus}
+              typeFile={ReportPaths.reportExcel}
+              typePathExcel="balanceGeneral"
+              level={2}
+            />
           </div>
         </div>
-        {/* aqui generar el reporte */}
-        <div className="flex gap-4 py-3 flex-row justify-end lg:flex-row w-full md:flex-col md:w-auto sm:justify-start">
-          <ReportGeneratorFile
-            dateRange={dateRange}
-            inSus={inSus}
-            reportNamePath="XlxsData"
-            paramType="balanceGeneral"
-            setFile={setPdfFile}
-            setGeneratedFiles={setGeneratedFiles}
-            setShowDialog={setShowDialog}
-          />
-          <ReportExcelGenerate
-            dateRange={dateRange}
-            inSus={inSus}
-            typeFile={ReportPaths.reportExcel}
-            typePathExcel="balanceGeneral"
-          />
+
+        {/* Descargar pdf */}
+        <div className="h-16 flex items-center">
+          {pdfFile && (
+            <ButtonLinkPDF
+              pdfFile={pdfFile}
+              nameFile="R_balance_general"
+            />
+          )}
         </div>
+        {/* Vista Previa */}
       </div>
-      {/* <Dialog open={showDialog} onOpenChange={setShowDialog}>
-        Comentado pero podria ser util si se requiere en algun momento
-        <DialogTrigger asChild>
-          <Button variant="outline" className="hidden">
-            Mostrar Links
-          </Button>
-        </DialogTrigger>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Archivos Generados</DialogTitle>
-            <DialogDescription>
-              Puedes descargar los reportes generados a continuación:
-            </DialogDescription>
-          </DialogHeader>
-          <div className="flex space-x-4">
-            {excelLink && (
-              <Button onClick={() => window.open(excelLink ?? "", "_self")}>
-                <Sheet className="mr-2 h-4 w-4" /> Descargar Excel
-              </Button>
-            )}
-            {pdfLink && (
-              <Button onClick={() => window.open(pdfLink ?? "", "_self")}>
-                <FileText className="mr-2 h-4 w-4" /> Descargar PDF
-              </Button>
-            )}
-          </div>
-        </DialogContent>
-      </Dialog> */}
-
-      {/* alternativa para descargar  */}
-      {/* <GeneratedFilesTable nameFile="l_diario" data={generatedFiles} /> */}
-
       {
-        pdfFile && (
-          <div style={{ height: "500px" }}>
-            <PDFViewer style={{ width: "100%", height: "100%" }}>
-              {pdfFile}
-            </PDFViewer>
+        dataBalanceGeneral && (
+          <div className="overflow-x-auto mx-auto w-[90vw] md:w-[900px] min-h-screen py-3">
+            <div className="flex px-2">
+              <BalanceGeneralPreview
+                dateRange={dateRange}
+                data={dataBalanceGeneral}
+              />
+            </div>
           </div>
         )
       }
-    </div>
+    </>
   );
 }
