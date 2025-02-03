a1337249fd2aff1458c2770e6d0e974db5c2de21
David Antezana, rats4final <rats4final@gmail.com>
2025-02-03 14:29:09 -0400

    fix(folders): Enviar voucher a FormNewVoucher desde FormNewVoucherWithTypeSelect

--- src/modules/shared/components/FormNewVoucherWithTypeSelect.tsx
+++ src/modules/shared/components/FormNewVoucherWithTypeSelect.tsx
@@ -63,6 +63,7 @@ export default function FormNewVoucherWithTypeSelect({
         bankExtractId={bankExtractId}
         gloss={gloss}
         voucherItemsFromExtractedPDF={validVoucherItemsPDF}
+        voucher={voucher}
       />
     </>
   );
