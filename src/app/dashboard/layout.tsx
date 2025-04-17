25fd05cc49d21795afea452f5e5448d4372b7fd3
Usuario6842 <136459003+Usuario6842@users.noreply.github.com>
2025-04-17 13:13:25 -0400

    feat: refactorizar el componente Layout intregrando el sideBar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,63 +2,35 @@
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { SideMenu } from "@/modules/shared/components/SideMenu";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import SideMenuMobile from "@/modules/shared/components/SideMenuMobile";
-import { Archive, Bolt, Menu } from "lucide-react";
+import { Bolt, } from "lucide-react";
 import { ModeToggle } from "@/components/mode-toggle";
 import UfvRegistrationDialog from "@/modules/ufv/components/UfvRegistrationDialog";
 import { useInitializeUserStore } from "@/modules/shared/hooks/useInitializeUserStore";
-import Image from "next/image";
-import { APP_LOGO_URL } from "@/lib/constants";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function Dashboard({ children }: { children: React.ReactNode }) {
   useInitializeUserStore();
 
   return (
-    <div className="grid max-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]  ">
-      <div className="hidden border-r bg-muted/40 md:block border-gray-700">
-        <div className="sticky flex max-h-screen flex-col gap-2">
-          <div className="flex h-14 items-center border-b px-4 justify-between lg:h-[60px] lg:px-6 border-gray-500">
-            <Link href="#" className="flex items-center gap-2 font-semibold">
-              {/* <Archive className="h-6 w-6" />
-              <span className="">Noxun</span> */}
-              <Image className="h-8 w-full" src={APP_LOGO_URL} width={3840} height={1040} alt="Logo" />
-            </Link>
+    <SidebarProvider>
+      <SideMenu />
+      <main className="flex flex-col gap-2">
+        <div className="w-full px-4 py-1 sticky top-0 z-50 bg-sidebar flex items-center justify-between max-w-full">
+          <SidebarTrigger className="size-10 p-[10px] text-black dark:text-white bg-white dark:bg-black border-[1px] border-black/10 dark:border-white/10" />
+          <div className="flex items-center gap-2">
             <ModeToggle />
-          </div>
-          <div className="flex-1 overflow-y-auto">
-            <SideMenu />
+            <Button asChild variant="outline" size="icon">
+              <Link className="sm:ml-auto" href="/dashboard/accounting/config">
+                <Bolt className="size-4" />
+              </Link>
+            </Button>
           </div>
         </div>
-      </div>
-      <div className="flex flex-col max-h-screen ">
-        <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6 border-gray-500">
-          <Sheet>
-            <SheetTrigger asChild>
-              <Button
-                variant="outline"
-                size="icon"
-                className="shrink-0 md:hidden"
-              >
-                <Menu className="h-5 w-5" />
-                <span className="sr-only">Toggle navigation menu</span>
-              </Button>
-            </SheetTrigger>
-            <SheetContent side="left" className="flex flex-col overflow-y-auto">
-              <SideMenuMobile />
-            </SheetContent>
-          </Sheet>
-          <Button asChild variant="outline" size="icon">
-            <Link className="sm:ml-auto" href="/dashboard/accounting/config">
-              <Bolt className="size-4" />
-            </Link>
-          </Button>
-        </header>
-        <main className="overflow-auto flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+        <div className="pl-5 max-w-full min-h-screen overflow-x-auto">
           {children}
-        </main>
-      </div>
+        </div>
+      </main>
       <UfvRegistrationDialog />
-    </div>
+    </SidebarProvider>
   );
 }
