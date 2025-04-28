6db4ef04223be561937de3aa6d6a75ab5d092394
David Antezana, rats4final <rats4final@gmail.com>
2025-04-28 08:17:07 -0400

    fix: Agregar parametros a fetchCostCenter en useCostCenter

--- src/modules/shared/hooks/useCostCenter.ts
+++ src/modules/shared/hooks/useCostCenter.ts
@@ -1,9 +1,16 @@
 import { fetchCostCenter } from "@/lib/data";
+import useUserStore from "@/lib/userStore";
 import { useQuery } from "@tanstack/react-query";
 
 export default function useCostCenter() {
+
+  const loginData = useUserStore((state) => state.loginData);
+  const filteredCostCenterClients = loginData?.rols.filter(
+    (item) => !item.isMenu
+  ) ?? [];
+
   return useQuery({
-    queryKey: ["costCenter"],
-    queryFn: fetchCostCenter,
+    queryKey: ["costCenter", filteredCostCenterClients],
+    queryFn: () => fetchCostCenter(filteredCostCenterClients),
   });
 }
