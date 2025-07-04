adc960bd75016f6af447fd9538a0e5ec69f05104
David Antezana <rats4final@gmail.com>
2025-07-04 01:11:14 -0400

    chore(sells): limpiar pagina de ventas

--- src/app/dashboard/accounting/invoice-registry/sells/page.tsx
+++ src/app/dashboard/accounting/invoice-registry/sells/page.tsx
@@ -1,23 +1,5 @@
-"use client"
-import { Button } from "@/components/ui/button";
-import DownloadMassPurchaseFormButton from "@/features/accounting/invoice-registry/components/DownloadMassTransactionFormButton";
-import ImportInvoiceRegistryDialog from "@/features/accounting/invoice-registry/components/ImportInvoiceRegistryDialog";
-import ListInvoiceRegistry from "@/features/accounting/invoice-registry/components/ListInvoiceRegistry";
-import Link from "next/link";
+"use client";
 
-export default function InvoiceRegistrySellsPage() {
-  const sells = 1;
-  return (
-    <main className="overflow-x-visible space-y-4">
-      <h1>Registro de Ventas</h1>
-      <div className="flex gap-6">
-        <Button asChild>
-          <Link href="/dashboard/accounting/invoice-registry/new">Nueva Registro</Link>
-        </Button>
-        <DownloadMassPurchaseFormButton type={sells}/>
-        <ImportInvoiceRegistryDialog/>
-      </div>
-      <ListInvoiceRegistry type={sells}/>
-    </main>
-  )
+export default function SellsPage() {
+  return <div>Ventas</div>;
 }
