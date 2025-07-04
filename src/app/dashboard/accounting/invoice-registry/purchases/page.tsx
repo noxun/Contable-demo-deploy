164d14daa0735f88228f3e5958d7925b249b0776
David Antezana <rats4final@gmail.com>
2025-07-04 15:06:27 -0400

    feat(purchases): agregar funcionalidad para subir la plantilla de compras

--- src/app/dashboard/accounting/invoice-registry/purchases/page.tsx
+++ src/app/dashboard/accounting/invoice-registry/purchases/page.tsx
@@ -3,6 +3,7 @@
 import { DownloadPurchasesOrSellsTemplateButton } from "@/features/accounting/invoice-registry/components/DownloadPurchasesOrSellsTemplateButton";
 import { DialogCreateOrUpdatePurchaseForm } from "@/features/accounting/invoice-registry/purchases/components/DialogCreateOrUpdatePurchaseForm";
 import ListPurchases from "@/features/accounting/invoice-registry/purchases/components/ListPurchases";
+import { UploadPurchaseTemplateDialog } from "@/features/accounting/invoice-registry/purchases/components/UploadPurchaseTemplateDialog";
 
 export default function PurchasesPage() {
   return (
@@ -10,6 +11,7 @@ export default function PurchasesPage() {
       <h1 className="text-2xl font-bold">Registro de Compras</h1>
       <div className="flex flex-col gap-4 p-4">
         <DownloadPurchasesOrSellsTemplateButton />
+        <UploadPurchaseTemplateDialog />
         <DialogCreateOrUpdatePurchaseForm mode="create" />
       </div>
       <div className="overflow-x-auto">
--- src/features/accounting/invoice-registry/purchases/services/purchasesService.ts
+++ src/features/accounting/invoice-registry/purchases/services/purchasesService.ts
@@ -4,6 +4,7 @@ import {
   Purchase,
   UpdatePurchase,
 } from "../schemas/purchaseSchema";
+import { UploadPurchaseTemplateSchema } from "../schemas/uploadPurchaseTemplateSchema";
 
 export const purchasesService = {
   async fetchPurchasesList() {
@@ -17,5 +18,15 @@ export const purchasesService = {
   async updatePurchase({ id, data }: { id: number; data: UpdatePurchase }) {
     const response = await api.put(`/api/Book/buy/${id}`, data);
     return response.data as Purchase;
+  },
+  async uploadPurchaseTemplate(data: UploadPurchaseTemplateSchema) {
+    const formData = new FormData();
+    formData.append("file", data.file);
+    const response = await api.post("/api/Book/buy/fill", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return response.data;
   },
 };
