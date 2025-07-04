9be463daadf722d6dae05821f69b73effb00901e
David Antezana <rats4final@gmail.com>
2025-07-04 01:09:43 -0400

    chore: eliminar modulos anteriores de compras y ventas

--- src/app/dashboard/accounting/invoice-registry/new/page.tsx
+++
@@ -1,5 +0,0 @@
-import InvoiceRegistryForm from "@/features/accounting/invoice-registry/components/FormNewInvoiceRegistry";
-
-export default function NewInvoiceRegistryPage() {
-  return <InvoiceRegistryForm />;
-}
