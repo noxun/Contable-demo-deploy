85e6a6ec3e2378c015c2f772e9a9fca6845ef3f9
David Antezana <rats4final@gmail.com>
2025-07-17 03:21:45 -0400

    feat(app): envolver la aplicacion en el adaptador de nuqs

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { NuqsAdapter } from 'nuqs/adapters/next/app'
 import React from "react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <>
-      {children}
+      <NuqsAdapter>{children}</NuqsAdapter>
       <ReactQueryDevtools initialIsOpen={false} />
     </>
   );
