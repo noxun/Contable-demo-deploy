abff41a85061e87b3114a603e7fc84ad90bf1b0d
David Antezana <rats4final@gmail.com>
2025-07-17 03:23:28 -0400

    feat(purchases): agregar los filtros al servicio de obtener todas las compras

--- src/features/accounting/invoice-registry/purchases/services/purchasesService.ts
+++ src/features/accounting/invoice-registry/purchases/services/purchasesService.ts
@@ -6,11 +6,48 @@ import {
 } from "../schemas/purchaseSchema";
 import { UploadPurchaseTemplateSchema } from "../schemas/uploadPurchaseTemplateSchema";
 import { ApplyAccountSchema } from "../schemas/applyAccountSchema";
+import { format, ParseISOOptions } from "date-fns";
+
+export type PaginationInvoice = {
+  CurrentPage: number;
+  ItemsPerPage: number;
+  TotalItems: number;
+  TotalPages: number;
+};
 
 export const purchasesService = {
-  async fetchPurchasesList() {
-    const response = await api.get("/api/Book/buy/list");
-    return response.data as Purchase[];
+  async fetchPurchasesList(
+    applyVoucher?: boolean,
+    orderByDesc?: boolean,
+    initDate?: Date,
+    endDate?: Date,
+    pageNumber?: number,
+    pageSize?: number
+  ) {
+    const parsedInitDate = initDate
+      ? format(initDate, "yyyy-MM-dd")
+      : undefined;
+    const parsedEndDate = endDate ? format(endDate, "yyyy-MM-dd") : undefined;
+
+    const response = await api.get("/api/Book/buy/list", {
+      params: {
+        applyVoucher,
+        orderByDesc,
+        parsedInitDate,
+        parsedEndDate,
+        pageNumber,
+        pageSize,
+      },
+    });
+
+    const pagination: PaginationInvoice = JSON.parse(
+      response.headers["pagination"] || "{}"
+    );
+
+    return {
+      data: response.data as Purchase[],
+      pagination,
+    };
   },
   async postPurchase(data: CreatePurchase) {
     const response = await api.post("/api/Book/buy", data);
@@ -47,9 +84,12 @@ export const purchasesService = {
     return response.data;
   },
   async downloadPurchaseTemplate() {
-    const response = await api.get("/api/templates/standard-purchase-template", {
-      responseType: "blob",
-    });
+    const response = await api.get(
+      "/api/templates/standard-purchase-template",
+      {
+        responseType: "blob",
+      }
+    );
     return response.data;
   },
 };
