fd25257546869b76fb8aba9495dd4f08654cdd78
David Antezana <rats4final@gmail.com>
2025-07-21 02:03:52 -0400

    feat(transactions): envolver ListVouchers en Suspense

--- src/app/dashboard/accounting/siat/transactions/page.tsx
+++ src/app/dashboard/accounting/siat/transactions/page.tsx
@@ -1,75 +1,18 @@
 "use client"; // Esto convierte el componente en un Client Component
-
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import SyncAccountsButton from "@/features/accounting/link/components/SyncAccountsButton";
 import ListVouchers from "@/features/accounting/vouchers/components/ListVouchers";
-import {
-  VoucherType,
-  VoucherTypeRoute,
-} from "@/features/accounting/shared/types/sharedTypes";
-import { useState } from "react";
-
-type VoucherInfo = {
-  label?: string;
-  voucherType: string;
-  voucherTypeRoute: string;
-};
-
-const voucherOptions: VoucherInfo[] = [
-  { label: "Traspaso", voucherType: "0", voucherTypeRoute: "diary" },
-  { label: "Egresos", voucherType: "1", voucherTypeRoute: "expenses" },
-  { label: "Ingresos", voucherType: "2", voucherTypeRoute: "income" },
-];
+import { Suspense } from "react";
 
 export default function SiatTransactionsPage() {
-  const [selectedOption, setSelectedOption] = useState<null | VoucherInfo>(
-    voucherOptions[0]
-  );
-
-  const handleSelectChange = (value: string) => {
-    const newValue: VoucherInfo = JSON.parse(value);
-    setSelectedOption(newValue);
-  };
-
   return (
     <section className="px-6">
       <div className="flex justify-between mb-2">
-        <SyncAccountsButton/>
+        <SyncAccountsButton />
         <h2 className="text-lg font-semibold">Transacciones Siat</h2>
-        <div>
-          <Select
-            // value={selectedOption}
-            onValueChange={handleSelectChange}
-          >
-            <SelectTrigger className="w-40">
-              <SelectValue placeholder="Seleccione el tipo de transaccion" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectContent>
-                {voucherOptions.map((option) => (
-                  <SelectItem
-                    key={option.voucherType}
-                    value={JSON.stringify(option)}
-                  >
-                    {option.label}
-                  </SelectItem>
-                ))}
-              </SelectContent>
-            </SelectContent>
-          </Select>
-        </div>
       </div>
-      <ListVouchers
-        voucherType={selectedOption?.voucherType as VoucherType}
-        voucherTypeRoute={selectedOption?.voucherTypeRoute as VoucherTypeRoute}
-        siat="siat"
-      />
+      <Suspense fallback={<div>Cargando...</div>}>
+        <ListVouchers siat="siat" />
+      </Suspense>
     </section>
   );
 }
--- src/app/dashboard/accounting/transactions/page.tsx
+++ src/app/dashboard/accounting/transactions/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import ListVouchers from "@/features/accounting/vouchers/components/ListVouchers";
+import { Suspense } from "react";
 
 export default function TransactionsPage() {
   return (
     <section className="px-6">
-      <ListVouchers/>
+      <Suspense fallback={<div>Cargando...</div>}>
+        <ListVouchers />
+      </Suspense>
     </section>
   );
 }
