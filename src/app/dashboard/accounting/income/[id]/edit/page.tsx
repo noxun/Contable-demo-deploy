5fa833eb4b8145fdf248ead58837eb848c4deb82
David Antezana <rats4final@gmail.com>
2025-07-23 12:38:31 -0400

    fix(app): reagregar paginas para edicion de vouchers

---
+++ src/app/dashboard/accounting/income/[id]/edit/page.tsx
@@ -0,0 +1,11 @@
+import EditVoucher from "@/features/accounting/shared/components/EditVoucher";
+import { VoucherType } from "@/features/accounting/shared/types/sharedTypes";
+
+export default function EditIncomePage({ params }: { params: { id: string } }) {
+  const { id } = params;
+  return (
+    <div className="px-5">
+      <EditVoucher id={id} type={VoucherType.INCOME} />
+    </div>
+  );
+}
