803a6c3ca93df4babd481470eaafc55268365c96
David Antezana <rats4final@gmail.com>
2025-08-05 02:37:32 -0400

    chore: borrar caracteres invalidos

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,7 +1,7 @@
 export const REPORTS_LOGO_URL = "https://res.cloudinary.com/ahsoizdev/image/upload/v1738081247/logos/yi1wazohyluhdfrvmuyn.png"
 export const APP_LOGO_URL = "/images/tradecruz_logo.png"; //puede ser una ruta relativa
 export const COMPANY_NAME = "TRADECRUZ S.R.L.";
-export const COMPANY_ADDRESS = "4to. Anillo entre Av. Banzer y Beni Calle Tristan Languidey Nro. 19";
+export const COMPANY_ADDRESS = "4to. Anillo entre Av. Banzer y Beni Calle Tristan Languidey Nro. 19";
 export const COMPANY_MAIN_CITY = "Santa Cruz";
 export const COMPANY_PHONES = ["(591)3-3406135","(591)3-385332","(591)3-3852168"];
 export const COMPANY_NIT = "3754820020";
