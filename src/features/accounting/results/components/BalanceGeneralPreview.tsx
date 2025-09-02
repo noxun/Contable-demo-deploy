da128d15f08a55192c404d1318ae151216c2b3b4
David Antezana <rats4final@gmail.com>
2025-09-02 18:00:17 -0400

    feat(reports): agregar link para ver el libro mayor de una cuenta si esta en el nivel 5 o 6 en balance general y estado de resultados

--- src/features/accounting/results/components/BalanceGeneralPreview.tsx
+++ src/features/accounting/results/components/BalanceGeneralPreview.tsx
@@ -3,85 +3,137 @@ import { formatNumber } from "@/features/accounting/shared/utils/validate";
 import { format } from "date-fns";
 import { BalanceGeneralType, BalanceItemType } from "../types/types";
 import { Fragment } from "react";
+import BiggerBookByAccountCodeDialog from "../../bigger-book/components/BiggerBookByAccountCodeDialog";
 
 interface Props {
-  data: BalanceGeneralType
-  dateRange: DateRange
-  currentLevel: number
-  inSus?: boolean
+  data: BalanceGeneralType;
+  dateRange: DateRange;
+  currentLevel: number;
+  inSus?: boolean;
 }
 
-export const BalanceGeneralPreview = ({ data, dateRange, currentLevel, inSus = false }: Props) => {
-  const FORMAT_DATE_INITIAL = 'dd/MM/yyyy';
-  const iDate = dateRange.from && format(dateRange.from, FORMAT_DATE_INITIAL)
-  const eDate = dateRange.to && format(dateRange.to, FORMAT_DATE_INITIAL)
+export const BalanceGeneralPreview = ({
+  data,
+  dateRange,
+  currentLevel,
+  inSus = false,
+}: Props) => {
+  const FORMAT_DATE_INITIAL = "dd/MM/yyyy";
+  const iDate = dateRange.from && format(dateRange.from, FORMAT_DATE_INITIAL);
+  const eDate = dateRange.to && format(dateRange.to, FORMAT_DATE_INITIAL);
   const moneyType = inSus ? "Dolares" : "Bolivianos";
 
-  const messageDate = iDate && eDate
-    ? (iDate === eDate ? `Del ${iDate}`
-      : `Del ${iDate} al ${eDate}`)
-    : `Del ${iDate}`
+  const messageDate =
+    iDate && eDate
+      ? iDate === eDate
+        ? `Del ${iDate}`
+        : `Del ${iDate} al ${eDate}`
+      : `Del ${iDate}`;
 
   const renderItem = (item: BalanceItemType, level = 1): JSX.Element => {
     return (
       <Fragment key={item.code}>
-        <tr className="hover:bg-blue-500/20 dark:hover:bg-blue-500/40" >
+        <tr className="hover:bg-blue-500/20 dark:hover:bg-blue-500/40">
           {/* <td>{item.code}</td> */}
-          <td style={{ paddingLeft: `${level * 10}px` }} className={`${currentLevel === level ? "font-normal" : "font-semibold"}`}>
-            {item.description}
+          <td
+            style={{ paddingLeft: `${level * 10}px` }}
+            className={`${
+              currentLevel === level ? "font-normal" : "font-semibold"
+            }`}
+          >
+            {item.level === 5 || item.level === 6 ? (
+              <BiggerBookByAccountCodeDialog
+                accountCode={item.code}
+                triggerType="link"
+              >
+                {item.description}
+              </BiggerBookByAccountCodeDialog>
+            ) : (
+              item.description
+            )}
           </td>
-          <td className={`text-end ${currentLevel === level ? "font-normal" : "font-semibold"}`}>{currentLevel === item.level ? formatNumber(item.sld) : " "}</td>
-          <td className={`text-end ${currentLevel === level ? "font-normal" : "font-semibold"}`}>{currentLevel - 1 === item.level ? formatNumber(item.sld) : " "}</td>
-          <td className={`text-end ${currentLevel === level ? "font-normal" : "font-semibold"}`}>{currentLevel - 2 === item.level ? formatNumber(item.sld) : " "}</td>
+          <td
+            className={`text-end ${
+              currentLevel === level ? "font-normal" : "font-semibold"
+            }`}
+          >
+            {currentLevel === item.level ? formatNumber(item.sld) : " "}
+          </td>
+          <td
+            className={`text-end ${
+              currentLevel === level ? "font-normal" : "font-semibold"
+            }`}
+          >
+            {currentLevel - 1 === item.level ? formatNumber(item.sld) : " "}
+          </td>
+          <td
+            className={`text-end ${
+              currentLevel === level ? "font-normal" : "font-semibold"
+            }`}
+          >
+            {currentLevel - 2 === item.level ? formatNumber(item.sld) : " "}
+          </td>
         </tr>
-        {item.itemsChild.length > 0 && item.itemsChild.map((child) => renderItem(child, level + 1))}
+        {item.itemsChild.length > 0 &&
+          item.itemsChild.map((child) => renderItem(child, level + 1))}
 
         {/* Los totales */}
         {level === 1 && (
           <>
-            {item.description === 'ACTIVO' && (
-            <>
-              <tr className="h-1"></tr>
-              <tr className="font-bold bg-gray-50 dark:bg-gray-800">
-                <td colSpan={2}>TOTAL ACTIVO</td>
-                <td colSpan={3} className="text-end">{formatNumber(data.totalActive)}</td>
-              </tr>
-              <tr className="h-6"></tr>
-            </>
-            )}
-            {item.description === 'PASIVOS' && (
+            {item.description === "ACTIVO" && (
               <>
                 <tr className="h-1"></tr>
                 <tr className="font-bold bg-gray-50 dark:bg-gray-800">
-                  <td colSpan={2}>TOTAL PASIVO</td>
-                  <td colSpan={3} className="text-end">{formatNumber(data.totalPassive)}</td>
+                  <td colSpan={2}>TOTAL ACTIVO</td>
+                  <td colSpan={3} className="text-end">
+                    {formatNumber(data.totalActive)}
+                  </td>
                 </tr>
                 <tr className="h-6"></tr>
               </>
             )}
-            {item.description === 'PATRIMONIO' && (
+            {item.description === "PASIVOS" && (
+              <>
+                <tr className="h-1"></tr>
+                <tr className="font-bold bg-gray-50 dark:bg-gray-800">
+                  <td colSpan={2}>TOTAL PASIVO</td>
+                  <td colSpan={3} className="text-end">
+                    {formatNumber(data.totalPassive)}
+                  </td>
+                </tr>
+                <tr className="h-6"></tr>
+              </>
+            )}
+            {item.description === "PATRIMONIO" && (
               <>
                 <tr className="h-2"></tr>
                 <tr className="font-bold border-gray-300 dark:bg-gray-800 bg-gray-50">
                   <td colSpan={2}>TOTAL PATRIMONIO</td>
-                  <td colSpan={3} className="text-right pr-4">{formatNumber(data.totalHeritage)}</td>
+                  <td colSpan={3} className="text-right pr-4">
+                    {formatNumber(data.totalHeritage)}
+                  </td>
                 </tr>
                 <tr className="font-bold border-gray-300 dark:bg-gray-800 border-t bg-gray-50">
                   <td colSpan={2}>RESULTADO</td>
-                  <td colSpan={3} className="text-right pr-4">{formatNumber(data.result)}</td>
+                  <td colSpan={3} className="text-right pr-4">
+                    {formatNumber(data.result)}
+                  </td>
                 </tr>
                 <tr className="font-bold border-gray-400 dark:bg-gray-800 bg-gray-50">
                   <td colSpan={2}>TOTAL + PATRIMONIO + RESULTADO</td>
-                  <td colSpan={3} className="text-right pr-4">{formatNumber(data.liabilityEquityResult)}</td>
+                  <td colSpan={3} className="text-right pr-4">
+                    {formatNumber(data.liabilityEquityResult)}
+                  </td>
                 </tr>
               </>
             )}
           </>
         )}
-      </Fragment>)
-  }
+      </Fragment>
+    );
+  };
 
-  if (data?.items.length <= 0) return <div>Sin Resultados</div>
+  if (data?.items.length <= 0) return <div>Sin Resultados</div>;
 
   return (
     <>
@@ -90,7 +142,12 @@ export const BalanceGeneralPreview = ({ data, dateRange, currentLevel, inSus = f
         <p className="w-full text-center">{messageDate}</p>
         <p className="w-full text-center text-sm">(Expresado en {moneyType})</p>
       </div>
-      <table className="w-full" border={1} cellPadding="5" style={{ borderCollapse: "collapse" }}>
+      <table
+        className="w-full"
+        border={1}
+        cellPadding="5"
+        style={{ borderCollapse: "collapse" }}
+      >
         <thead>
           {/* <tr className="bg-[#E0E0E0] dark:text-[#4a4a4a]">
             <th className="font-semibold">Cuenta</th>
@@ -100,11 +157,8 @@ export const BalanceGeneralPreview = ({ data, dateRange, currentLevel, inSus = f
             <th className="font-semibold">{(currentLevel - 2) <= 0 ? ' ' : `Imp. Niv. ${currentLevel - 2}`}</th>
           </tr> */}
         </thead>
-        <tbody>
-          {data.items.map((item) => renderItem(item))}
-        </tbody>
+        <tbody>{data.items.map((item) => renderItem(item))}</tbody>
       </table>
     </>
   );
-
 };
